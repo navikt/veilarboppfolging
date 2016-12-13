7ce312ea554ae6a4e9eda805ba1cbd1c64edc4d0
Joanna Eriksson <joanna.eriksson@nav.no>
2016-12-13 13:26:58 +0100

    prøver å sette opp Jetty

---
+++ web/src/main/java/no/nav/veilarbsituasjon/config/ApplicationConfig.java
@@ -0,0 +1,12 @@
+package no.nav.veilarbsituasjon.config;
+
+import org.springframework.context.annotation.*;
+
+//@EnableAspectJAutoProxy
+//@Configuration
+//@Import({
+//})
+@ComponentScan(basePackages = "no.nav.fo.modia.rest")
+public class ApplicationConfig {
+
+}
