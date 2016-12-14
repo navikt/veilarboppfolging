3fe5810f71858d9166e791c83a5fb0f8b1894321
Joanna Eriksson <joanna.eriksson@nav.no>
2016-12-14 12:06:58 +0100

    Basic oppsett av Spring boot og Jetty med enkelt rest-kall

---
+++ web/src/main/java/no/nav/veilarbsituasjon/rest/HelloController.java
@@ -0,0 +1,14 @@
+package no.nav.veilarbsituasjon.rest;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String index() {
+        return "Greetings from Modia Ytelseoversikt";
+    }
+}
