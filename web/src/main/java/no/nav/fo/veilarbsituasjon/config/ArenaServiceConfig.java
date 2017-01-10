04878a19c9673c03a8ba369b405b327d8ac118bc
Joanna Eriksson <joanna.eriksson@nav.no>
2017-01-10 15:35:46 +0100

    Tar i bruk egensignert SAML-token

--- web/src/main/java/no/nav/fo/veilarbsituasjon/config/ArenaServiceConfig.java
+++ web/src/main/java/no/nav/fo/veilarbsituasjon/config/ArenaServiceConfig.java
@@ -2,29 +2,34 @@ package no.nav.fo.veilarbsituasjon.config;
 
 
 import no.nav.fo.veilarbsituasjon.mock.YtelseskontraktV3Mock;
+import no.nav.modig.security.ws.SystemSAMLOutInterceptor;
 import no.nav.sbl.dialogarena.common.cxf.CXFClient;
 import no.nav.tjeneste.virksomhet.ytelseskontrakt.v3.YtelseskontraktV3;
+import org.slf4j.Logger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import static java.lang.System.getProperty;
 import static no.nav.sbl.dialogarena.common.cxf.InstanceSwitcher.createMetricsProxyWithInstanceSwitcher;
+import static org.slf4j.LoggerFactory.getLogger;
 
 
 @Configuration
 public class ArenaServiceConfig {
     private static final String ARENASERVICE_YTELSESKONTRAKT_MOCK_KEY = "ytelseskontrakt.arenaservice.withmock";
-
+    private static final Logger LOG = getLogger(ArenaServiceConfig.class);
 
     @Bean
     public YtelseskontraktV3 ytelseskontraktV3() {
-        YtelseskontraktV3 prod = ytelseskontraktPortType().configureStsForSystemUser().build();
-//                .withOutInterceptor(new SystemSAMLOutInterceptor()).build();
+        YtelseskontraktV3 prod = ytelseskontraktPortType().withOutInterceptor(new SystemSAMLOutInterceptor()).build();
         YtelseskontraktV3 mock = new YtelseskontraktV3Mock();
         return createMetricsProxyWithInstanceSwitcher("Ytelseskontrakt-ArenaService", prod, mock, ARENASERVICE_YTELSESKONTRAKT_MOCK_KEY, YtelseskontraktV3.class);
     }
 
     private CXFClient<YtelseskontraktV3> ytelseskontraktPortType() {
-        return new CXFClient<>(YtelseskontraktV3.class).address(getProperty("ytelseskontrakt.endpoint.url"));
+        final String url = getProperty("ytelseskontrakt.endpoint.url");
+        LOG.info("URL for Ytelsekontrakt_v3 er {}", url);
+
+        return new CXFClient<>(YtelseskontraktV3.class).address(url);
     }
 }
