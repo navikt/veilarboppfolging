85d76777e51fbc81a0807579104fee8f661c4035
Joanna Eriksson <joanna.eriksson@nav.no>
2017-01-10 13:07:33 +0100

    logging

--- web/src/main/java/no/nav/fo/veilarbsituasjon/services/YtelseskontraktService.java
+++ web/src/main/java/no/nav/fo/veilarbsituasjon/services/YtelseskontraktService.java
@@ -1,19 +1,22 @@
 package no.nav.fo.veilarbsituasjon.services;
 
 
-import no.nav.fo.veilarbsituasjon.rest.domain.Ytelsekontrakt;
 import no.nav.tjeneste.virksomhet.ytelseskontrakt.v3.HentYtelseskontraktListeSikkerhetsbegrensning;
 import no.nav.tjeneste.virksomhet.ytelseskontrakt.v3.YtelseskontraktV3;
 import no.nav.tjeneste.virksomhet.ytelseskontrakt.v3.informasjon.ytelseskontrakt.WSPeriode;
-import no.nav.tjeneste.virksomhet.ytelseskontrakt.v3.informasjon.ytelseskontrakt.WSYtelseskontrakt;
 import no.nav.tjeneste.virksomhet.ytelseskontrakt.v3.meldinger.WSHentYtelseskontraktListeRequest;
 import no.nav.tjeneste.virksomhet.ytelseskontrakt.v3.meldinger.WSHentYtelseskontraktListeResponse;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.xml.datatype.XMLGregorianCalendar;
-import java.util.List;
+
+import static org.slf4j.LoggerFactory.getLogger;
 
 public class YtelseskontraktService {
+
+    private static final Logger LOG = getLogger(YtelseskontraktService.class);
+
 
     @Autowired
     private YtelseskontraktV3 ytelseskontraktV3;
@@ -26,6 +29,7 @@ public class YtelseskontraktService {
                 .withPeriode(periode)
                 .withPersonidentifikator(personId);
         try {
+            LOG.info("Sender request til Ytelsekontrakt_v3");
             final WSHentYtelseskontraktListeResponse response = ytelseskontraktV3.hentYtelseskontraktListe(request);
 //            response.getYtelseskontraktListe().stream().forEach(WSYtelseskontrakt);
         } catch (HentYtelseskontraktListeSikkerhetsbegrensning hentYtelseskontraktListeSikkerhetsbegrensning) {
