4ab130b1a050e7732dc1c584528bd2066cab391f
Joanna Eriksson <joanna.eriksson@nav.no>
2017-01-17 15:08:20 +0100

    Mapper response-objekt fra Ytelseskontrakt_v3 til objekt returneres av rest

---
+++ web/src/main/java/no/nav/fo/veilarbsituasjon/rest/domain/Ytelseskontrakt.java
@@ -0,0 +1,58 @@
+package no.nav.fo.veilarbsituasjon.rest.domain;
+
+
+import javax.xml.datatype.XMLGregorianCalendar;
+
+class Ytelseskontrakt {
+
+    private String status;
+
+    private String ytelsestype;
+    private Dato datoMottat;
+    private Dato datoFra;
+    private Dato datoTil;
+
+    public String getYtelsestype() {
+        return ytelsestype;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public Dato getDatoMottat() {
+        return datoMottat;
+    }
+
+    public Dato getDatoFra() {
+        return datoFra;
+    }
+
+    public Dato getDatoTil() {
+        return datoTil;
+    }
+
+
+    Ytelseskontrakt withYtelsestype(String ytelsestype) {
+        this.ytelsestype = ytelsestype;
+        return this;
+    }
+
+    Ytelseskontrakt withStatus(String status) {
+        this.status = status;
+        return this;
+    }
+
+    Ytelseskontrakt withDatoMottat(XMLGregorianCalendar datoKravMottatt) {
+        datoMottat = new Dato(datoKravMottatt.getYear(), datoKravMottatt.getMonth(), datoKravMottatt.getDay());
+        return this;
+    }
+
+    void setDatoFra(XMLGregorianCalendar datoFra) {
+        this.datoFra = new Dato(datoFra.getYear(), datoFra.getMonth(), datoFra.getDay());
+    }
+
+    void setDatoTil(XMLGregorianCalendar datoTil) {
+        this.datoTil = new Dato(datoTil.getYear(), datoTil.getMonth(), datoTil.getDay());
+    }
+}
