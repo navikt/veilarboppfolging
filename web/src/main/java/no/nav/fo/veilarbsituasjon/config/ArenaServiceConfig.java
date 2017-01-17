72274b6982dcb73925e54c00607cf9e09a8f68d3
Joanna Eriksson <joanna.eriksson@nav.no>
2017-01-17 17:12:27 +0100

    Flyttet ApplicationConfig-fil og lagt til selftest

--- web/src/main/java/no/nav/fo/veilarbsituasjon/config/ArenaServiceConfig.java
+++ web/src/main/java/no/nav/fo/veilarbsituasjon/config/ArenaServiceConfig.java
@@ -2,7 +2,9 @@ package no.nav.fo.veilarbsituasjon.config;
 
 
 import no.nav.fo.veilarbsituasjon.mock.YtelseskontraktV3Mock;
+import no.nav.modig.security.ws.SystemSAMLOutInterceptor;
 import no.nav.sbl.dialogarena.common.cxf.CXFClient;
+import no.nav.sbl.dialogarena.types.Pingable;
 import no.nav.tjeneste.virksomhet.ytelseskontrakt.v3.YtelseskontraktV3;
 import org.slf4j.Logger;
 import org.springframework.context.annotation.Bean;
@@ -10,6 +12,8 @@ import org.springframework.context.annotation.Configuration;
 
 import static java.lang.System.getProperty;
 import static no.nav.sbl.dialogarena.common.cxf.InstanceSwitcher.createMetricsProxyWithInstanceSwitcher;
+import static no.nav.sbl.dialogarena.types.Pingable.Ping.feilet;
+import static no.nav.sbl.dialogarena.types.Pingable.Ping.lyktes;
 import static org.slf4j.LoggerFactory.getLogger;
 
 
@@ -31,5 +35,18 @@ public class ArenaServiceConfig {
         LOG.info("URL for Ytelsekontrakt_v3 er {}", url);
 
         return new CXFClient<>(YtelseskontraktV3.class).address(url);
+    }
+
+    @Bean
+    Pingable ytelseskontraktPing() {
+        final YtelseskontraktV3 ytelseskontraktPing = ytelseskontraktPortType().withOutInterceptor(new SystemSAMLOutInterceptor()).ikkeMaskerSecurityHeader().build();
+        return () -> {
+            try {
+                ytelseskontraktPing.ping();
+                return lyktes("YTELSESKONTRAKT_V3");
+            } catch (Exception e) {
+                return feilet("YTELSESKONTRAKT_V3", e);
+            }
+        };
     }
 }
