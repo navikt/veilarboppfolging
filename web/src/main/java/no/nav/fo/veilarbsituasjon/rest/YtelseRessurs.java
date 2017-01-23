6b5ae682609b8f00f2722eb61ae92bd69622d8d2
Joanna Eriksson <joanna.eriksson@nav.no>
2017-01-23 08:06:20 +0100

    Returnerer innsatsgruppe og status ved rest-kall til ytelser

--- web/src/main/java/no/nav/fo/veilarbsituasjon/rest/YtelseRessurs.java
+++ web/src/main/java/no/nav/fo/veilarbsituasjon/rest/YtelseRessurs.java
@@ -1,12 +1,13 @@
 package no.nav.fo.veilarbsituasjon.rest;
 
-import no.nav.fo.veilarbsituasjon.rest.domain.YtelseskontraktResponse;
+import no.nav.fo.veilarbsituasjon.rest.domain.*;
+import no.nav.fo.veilarbsituasjon.services.OppfoelgingService;
 import no.nav.fo.veilarbsituasjon.services.YtelseskontraktService;
 import org.slf4j.Logger;
 import org.springframework.web.bind.annotation.*;
 
-import javax.xml.datatype.*;
-import java.time.*;
+import javax.xml.datatype.XMLGregorianCalendar;
+import java.time.LocalDate;
 
 import static no.nav.fo.veilarbsituasjon.utils.CalendarConverter.convertDateToXMLGregorianCalendar;
 import static org.slf4j.LoggerFactory.getLogger;
@@ -18,19 +19,29 @@ public class YtelseRessurs {
     private static final Logger LOG = getLogger(YtelseRessurs.class);
 
     final private YtelseskontraktService ytelseskontraktService;
+    final private OppfoelgingService oppfoelgingService;
 
-    public YtelseRessurs(YtelseskontraktService ytelseskontraktService) {
+
+    public YtelseRessurs(YtelseskontraktService ytelseskontraktService, OppfoelgingService oppfoelgingService) {
         this.ytelseskontraktService = ytelseskontraktService;
+        this.oppfoelgingService = oppfoelgingService;
     }
 
     @RequestMapping(value = "/ytelser", method = RequestMethod.GET, produces = "application/json")
-    public YtelseskontraktResponse getYtelser(@PathVariable String fnr) {
+    public YtelserResponse getYtelser(@PathVariable String fnr) {
         LocalDate periodeFom = LocalDate.of(2005, 10, 10);
         LocalDate periodeTom = LocalDate.now();
         XMLGregorianCalendar fom = convertDateToXMLGregorianCalendar(periodeFom);
         XMLGregorianCalendar tom = convertDateToXMLGregorianCalendar(periodeTom);
 
         LOG.info("Henter ytelse for {}", fnr);
-        return ytelseskontraktService.hentYtelseskontraktListe(fom, tom, fnr);
+        final YtelseskontraktResponse ytelseskontraktResponse = ytelseskontraktService.hentYtelseskontraktListe(fom, tom, fnr);
+        final OppfoelgingskontraktResponse oppfoelgingskontraktResponse = oppfoelgingService.hentOppfoelgingskontraktListe(fom, tom, fnr);
+
+        return new YtelserResponse()
+                .withVedtaksliste(ytelseskontraktResponse.getVedtaksliste())
+                .withYtelser(ytelseskontraktResponse.getYtelser())
+                .withInnsatsgruppe(oppfoelgingskontraktResponse.getOppfoelgingskontrakter());
+
     }
 }
---
+++ web/src/main/java/no/nav/fo/veilarbsituasjon/rest/domain/YtelserResponse.java
@@ -0,0 +1,37 @@
+package no.nav.fo.veilarbsituasjon.rest.domain;
+
+
+import java.util.List;
+
+public class YtelserResponse {
+    private List<Vedtak> vedtaksliste;
+    private List<Ytelseskontrakt> ytelser;
+    private List<Oppfoelgingskontrakt> oppfoelgingskontrakter;
+
+    public YtelserResponse withVedtaksliste(List<Vedtak> vedtaksliste) {
+        this.vedtaksliste = vedtaksliste;
+        return this;
+    }
+
+    public YtelserResponse withYtelser(List<Ytelseskontrakt> ytelser) {
+        this.ytelser = ytelser;
+        return this;
+    }
+
+    public YtelserResponse withInnsatsgruppe(List<Oppfoelgingskontrakt> oppfoelgingskontrakter) {
+        this.oppfoelgingskontrakter = oppfoelgingskontrakter;
+        return this;
+    }
+
+    public List<Vedtak> getVedtaksliste() {
+        return vedtaksliste;
+    }
+
+    public List<Ytelseskontrakt> getYtelser() {
+        return ytelser;
+    }
+
+    public List<Oppfoelgingskontrakt> getOppfoelgingskontrakter() {
+        return oppfoelgingskontrakter;
+    }
+}
