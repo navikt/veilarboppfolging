b1b762c39c465e6c190f4fd98f23e8ad561a70f3
Joanna Eriksson <joanna.eriksson@nav.no>
2017-03-13 09:30:57 +0100

    PK-38701 PKULV-2141 PKULV-2147: Tar i bruk PEP mot ABAC

--- web/src/main/java/no/nav/fo/veilarbsituasjon/rest/YtelseRessurs.java
+++ web/src/main/java/no/nav/fo/veilarbsituasjon/rest/YtelseRessurs.java
@@ -1,8 +1,12 @@
 package no.nav.fo.veilarbsituasjon.rest;
 
+import no.nav.brukerdialog.security.context.SubjectHandler;
 import no.nav.fo.veilarbsituasjon.rest.domain.*;
 import no.nav.fo.veilarbsituasjon.services.OppfolgingService;
 import no.nav.fo.veilarbsituasjon.services.YtelseskontraktService;
+import no.nav.sbl.dialogarena.common.abac.pep.Pep;
+import no.nav.sbl.dialogarena.common.abac.pep.domain.response.BiasedDecisionResponse;
+import no.nav.sbl.dialogarena.common.abac.pep.domain.response.Decision;
 import org.slf4j.Logger;
 import org.springframework.stereotype.Component;
 
@@ -24,16 +28,25 @@ public class YtelseRessurs {
 
     final private YtelseskontraktService ytelseskontraktService;
     final private OppfolgingService oppfolgingService;
+    final private Pep pep;
 
 
-    public YtelseRessurs(YtelseskontraktService ytelseskontraktService, OppfolgingService oppfolgingService) {
+    public YtelseRessurs(YtelseskontraktService ytelseskontraktService, OppfolgingService oppfolgingService, Pep pep) {
         this.ytelseskontraktService = ytelseskontraktService;
         this.oppfolgingService = oppfolgingService;
+        this.pep = pep;
     }
 
     @GET
     @Path("/ytelser")
     public YtelserResponse getYtelser(@PathParam("fnr") String fnr) {
+
+        final String ident = SubjectHandler.getSubjectHandler().getUid();
+        BiasedDecisionResponse callAllowed = pep.isServiceCallAllowedWithIdent(ident, "veilarb", fnr);
+        if (callAllowed.getBiasedDecision().equals(Decision.Deny)) {
+            throw new NotAuthorizedException(ident + " doesn't have access to " + fnr);
+        }
+
         LocalDate periodeFom = LocalDate.now().minusMonths(MANEDER_BAK_I_TID);
         LocalDate periodeTom = LocalDate.now().plusMonths(MANEDER_FREM_I_TID);
         XMLGregorianCalendar fom = convertDateToXMLGregorianCalendar(periodeFom);
--- web/src/test/java/no/nav/fo/veilarbsituasjon/rest/YtelsesRessursTest.java
+++ web/src/test/java/no/nav/fo/veilarbsituasjon/rest/YtelsesRessursTest.java
@@ -1,14 +1,21 @@
 package no.nav.fo.veilarbsituasjon.rest;
 
+import no.nav.brukerdialog.security.context.SubjectHandlerUtils;
+import no.nav.brukerdialog.security.context.ThreadLocalSubjectHandler;
+import no.nav.brukerdialog.security.domain.IdentType;
 import no.nav.fo.veilarbsituasjon.rest.domain.*;
 import no.nav.fo.veilarbsituasjon.services.OppfolgingService;
 import no.nav.fo.veilarbsituasjon.services.YtelseskontraktService;
+import no.nav.sbl.dialogarena.common.abac.pep.Pep;
+import no.nav.sbl.dialogarena.common.abac.pep.domain.response.*;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import static java.lang.System.setProperty;
 import static java.util.Collections.singletonList;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
@@ -28,6 +35,17 @@ public class YtelsesRessursTest {
     @Mock
     private OppfolgingService oppfolgingService;
 
+    @Mock
+    Pep pep;
+
+    @BeforeClass
+    public static void setup() {
+        setProperty("no.nav.modig.security.systemuser.username", "username");
+        setProperty("no.nav.modig.security.systemuser.password", "password");
+        setProperty("no.nav.modig.core.context.subjectHandlerImplementationClass", ThreadLocalSubjectHandler.class.getName());
+        SubjectHandlerUtils.setSubject(new SubjectHandlerUtils.SubjectBuilder("userId", IdentType.InternBruker).withAuthLevel(3).getSubject());
+    }
+
     @Test
     public void getOppfoelgingSkalReturnereEnRespons() throws Exception {
 
@@ -37,6 +55,9 @@ public class YtelsesRessursTest {
         when(oppfolgingService.hentOppfolgingskontraktListe(any(), any(), anyString())).thenReturn(
                 new OppfolgingskontraktResponse(singletonList(new OppfolgingskontraktData()))
         );
+
+        when(pep.isServiceCallAllowedWithIdent(anyString(), anyString(), anyString()))
+                .thenReturn(new BiasedDecisionResponse(Decision.Permit, new XacmlResponse()));
 
         final YtelserResponse ytelser = ytelseRessurs.getYtelser("***REMOVED***");
 
