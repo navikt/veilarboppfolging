bd06bbb6b5e9400fc92401a663f4349eb5dfabdf
Joanna Eriksson <joanna.eriksson@nav.no>
2017-03-16 13:33:10 +0100

    PK-38701 PKULV-2141 PKULV-2147: Bruker siste versjonen av common-abac og logger feil fra ABAC

--- web/src/main/java/no/nav/fo/veilarbsituasjon/services/PepClient.java
+++ web/src/main/java/no/nav/fo/veilarbsituasjon/services/PepClient.java
@@ -5,13 +5,17 @@ import no.nav.sbl.dialogarena.common.abac.pep.Pep;
 import no.nav.sbl.dialogarena.common.abac.pep.domain.response.BiasedDecisionResponse;
 import no.nav.sbl.dialogarena.common.abac.pep.domain.response.Decision;
 import no.nav.sbl.dialogarena.common.abac.pep.exception.PepException;
+import org.slf4j.Logger;
 
 import javax.ws.rs.InternalServerErrorException;
 import javax.ws.rs.NotAuthorizedException;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
 public class PepClient {
 
     final private Pep pep;
+    private static final Logger LOG = getLogger(PepClient.class);
 
     public PepClient(Pep pep) {
         this.pep = pep;
@@ -23,6 +27,7 @@ public class PepClient {
         try {
             callAllowed = pep.isServiceCallAllowedWithIdent(ident, "veilarb", fnr);
         } catch (PepException e) {
+            LOG.error("Something went wrong in PEP", e);
             throw new InternalServerErrorException("something went wrong in PEP", e);
         }
         if (callAllowed.getBiasedDecision().equals(Decision.Deny)) {
