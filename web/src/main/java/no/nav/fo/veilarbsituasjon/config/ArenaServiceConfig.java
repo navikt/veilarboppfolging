4f14d1a6ce9d164438fc00fcc0dea253102cc32c
Christian Finstad <christian.finstad@nav.no>
2017-04-19 14:46:38 +0200

    [F17HL2-285] Fjerner pingene til arena.

--- web/src/main/java/no/nav/fo/veilarbsituasjon/config/ArenaServiceConfig.java
+++ web/src/main/java/no/nav/fo/veilarbsituasjon/config/ArenaServiceConfig.java
@@ -3,9 +3,7 @@ package no.nav.fo.veilarbsituasjon.config;
 
 import no.nav.fo.veilarbsituasjon.mock.OppfoelgingV1Mock;
 import no.nav.fo.veilarbsituasjon.mock.YtelseskontraktV3Mock;
-import no.nav.modig.security.ws.SystemSAMLOutInterceptor;
 import no.nav.sbl.dialogarena.common.cxf.CXFClient;
-import no.nav.sbl.dialogarena.types.Pingable;
 import no.nav.tjeneste.virksomhet.oppfoelging.v1.OppfoelgingPortType;
 import no.nav.tjeneste.virksomhet.ytelseskontrakt.v3.YtelseskontraktV3;
 import org.slf4j.Logger;
@@ -14,8 +12,6 @@ import org.springframework.context.annotation.Configuration;
 
 import static java.lang.System.getProperty;
 import static no.nav.sbl.dialogarena.common.cxf.InstanceSwitcher.createMetricsProxyWithInstanceSwitcher;
-import static no.nav.sbl.dialogarena.types.Pingable.Ping.feilet;
-import static no.nav.sbl.dialogarena.types.Pingable.Ping.lyktes;
 import static org.slf4j.LoggerFactory.getLogger;
 
 @Configuration
@@ -53,38 +49,5 @@ public class ArenaServiceConfig {
         LOG.info("URL for Oppfoelging_V1 er {}", url);
         return new CXFClient<>(OppfoelgingPortType.class)
                 .address(url);
-    }
-
-    @Bean
-    Pingable ytelseskontraktPing() {
-        final YtelseskontraktV3 ytelseskontraktPing = ytelseskontraktPortType()
-                .withOutInterceptor(new SystemSAMLOutInterceptor())
-                .build();
-        return () -> {
-            try {
-                ytelseskontraktPing.ping();
-                return lyktes("YTELSESKONTRAKT_V3");
-            } catch (Exception e) {
-                return feilet("YTELSESKONTRAKT_V3", e);
-            }
-        };
-    }
-
-    @Bean
-    Pingable oppfoelgingPing() {
-        final String url = getProperty("oppfoelging.endpoint.url");
-        LOG.info("URL for Oppfoelging_V1 er {}", url);
-        OppfoelgingPortType oppfoelgingPing = oppfoelgingPortType()
-                .withOutInterceptor(new SystemSAMLOutInterceptor())
-                .build();
-
-        return () -> {
-            try {
-                oppfoelgingPing.ping();
-                return lyktes("OPPFOELGING_V1");
-            } catch (Exception e) {
-                return feilet("OPPFOELGING_V1", e);
-            }
-        };
     }
 }
