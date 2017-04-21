4c56d632aacd3f883f11bb5e79b5c3f711bd1d55
Peder Korsveien <peder.korsveien@nav.no>
2017-04-21 09:50:40 +0200

    Endre loggnivå

--- web/src/main/java/no/nav/fo/veilarbsituasjon/rest/PortefoljeRessurs.java
+++ web/src/main/java/no/nav/fo/veilarbsituasjon/rest/PortefoljeRessurs.java
@@ -76,13 +76,13 @@ public class PortefoljeRessurs {
             }
 
         } catch (JMSException e) {
-            LOG.error("Kunne ikke legge brukere på kø", e);
+            LOG.warn("Kunne ikke legge brukere på kø", e);
             return Response.serverError().entity("Kunne ikke legge brukere på kø").build();
         } catch (SQLException e) {
-            LOG.error("Kunne ikke skrive brukere til database", e);
+            LOG.warn("Kunne ikke skrive brukere til database", e);
             return Response.serverError().entity("Kunne ikke skrive brukere til database").build();
         } catch (Exception e) {
-            LOG.error("Kunne ikke tildele veileder", e);
+            LOG.warn("Kunne ikke tildele veileder", e);
             return Response.serverError().entity("Kunne ikke tildele veileder").build();
         }
     }
