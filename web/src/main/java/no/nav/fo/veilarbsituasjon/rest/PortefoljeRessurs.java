a4dc4ef41b81543d3aa42dd812550b9923128bac
Peder Korsveien <peder.korsveien@nav.no>
2017-05-22 13:06:27 +0200

    Oppdater pom.xml

--- web/src/main/java/no/nav/fo/veilarbsituasjon/rest/PortefoljeRessurs.java
+++ web/src/main/java/no/nav/fo/veilarbsituasjon/rest/PortefoljeRessurs.java
@@ -8,18 +8,17 @@ import no.nav.fo.veilarbsituasjon.rest.domain.TilordneVeilederResponse;
 import no.nav.fo.veilarbsituasjon.rest.domain.VeilederTilordning;
 import no.nav.fo.veilarbsituasjon.services.AktoerIdService;
 import no.nav.fo.veilarbsituasjon.services.PepClient;
+import no.nav.sbl.dialogarena.common.abac.pep.exception.PepException;
 import org.slf4j.Logger;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 import java.util.ArrayList;
 import java.util.List;
 
+import static java.util.Optional.ofNullable;
 import static org.slf4j.LoggerFactory.getLogger;
 
 @Component
@@ -51,29 +50,47 @@ public class PortefoljeRessurs {
         feilendeTilordninger = new ArrayList<>();
 
         for (VeilederTilordning tilordning : tilordninger) {
+            try {
+                final String fnr = tilordning.getBrukerFnr();
+                pepClient.isServiceCallAllowed(fnr);
 
-            final String fnr = tilordning.getBrukerFnr();
-            pepClient.isServiceCallAllowed(fnr);
-            String aktoerId = aktoerIdService.findAktoerId(fnr);
+                String aktoerId = ofNullable(aktoerIdService.findAktoerId(fnr)).
+                        orElseThrow(() -> new IllegalArgumentException("Aktoerid ikke funnet"));
 
-            OppfolgingBruker bruker = new OppfolgingBruker()
-                    .setVeileder(tilordning.getTilVeilederId())
-                    .setAktoerid(aktoerId);
 
-            settVeilederDersomFraVeilederErOK(bruker, tilordning);
+                OppfolgingBruker bruker = new OppfolgingBruker()
+                        .setVeileder(tilordning.getTilVeilederId())
+                        .setAktoerid(aktoerId);
+
+                settVeilederDersomFraVeilederErOK(bruker, tilordning);
+                feed.activateWebhook();
+            }catch(PepException e){
+                LOG.error("Kall til ABAC feilet");
+                feilendeTilordninger.add(tilordning);
+            }
+            catch(IllegalArgumentException e) {
+                LOG.error("Aktoerid ikke funnet", e);
+                feilendeTilordninger.add(tilordning);
+            }catch(NotAuthorizedException e) {
+                LOG.warn("Request is not authorized", e);
+                feilendeTilordninger.add(tilordning);
+            }catch(Exception e) {
+                LOG.error("Det skjedde en feil ved tildeling av veileder",e);
+                feilendeTilordninger.add(tilordning);
+            }
         }
 
         TilordneVeilederResponse response = new TilordneVeilederResponse()
                 .setFeilendeTilordninger(feilendeTilordninger);
 
         if (feilendeTilordninger.isEmpty()) {
-            response.setResultat("Veiledere tilordnet!");
-            feed.activateWebhook();
+            response.setResultat("OK: Veiledere tilordnet");
             return Response.ok().entity(response).build();
         } else {
-            response.setResultat("Noen brukere kunne ikke tilordnes en veileder.");
+            response.setResultat("WARNING: Noen brukere kunne ikke tilordnes en veileder");
             return Response.ok().entity(response).build();
         }
+
     }
 
 
