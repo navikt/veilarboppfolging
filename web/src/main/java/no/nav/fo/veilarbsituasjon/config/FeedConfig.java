32d90ae66ed2403aad5463eddda66bf74388c777
Christian Finstad <christian.finstad@nav.no>
2017-05-29 08:56:26 +0200

    Legger til OidcOutInterceptor FeedProduceren

--- web/src/main/java/no/nav/fo/veilarbsituasjon/config/FeedConfig.java
+++ web/src/main/java/no/nav/fo/veilarbsituasjon/config/FeedConfig.java
@@ -1,5 +1,6 @@
 package no.nav.fo.veilarbsituasjon.config;
 
+import no.nav.brukerdialog.security.oidc.OidcFeedOutInterceptor;
 import no.nav.fo.feed.controller.FeedController;
 import no.nav.fo.feed.producer.FeedProducer;
 import no.nav.fo.veilarbsituasjon.db.BrukerRepository;
@@ -7,6 +8,9 @@ import no.nav.fo.veilarbsituasjon.domain.OppfolgingBruker;
 import no.nav.fo.veilarbsituasjon.services.TilordningFeedProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import static java.util.Collections.singletonList;
+
 
 @Configuration
 public class FeedConfig {
@@ -25,6 +29,7 @@ public class FeedConfig {
         return FeedProducer.<OppfolgingBruker>builder()
                 .provider(new TilordningFeedProvider(brukerRepository))
                 .maxPageSize(1000)
+                .interceptors(singletonList(new OidcFeedOutInterceptor()) )
                 .build();
     }
 }
