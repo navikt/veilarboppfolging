85f83654704e288694d9c9c0f4702dfb41e79167
Martin Solheim <martin.solheim@nav.no>
2017-08-17 09:55:06 +0200

    PKULV-3167 Flytter gjeldende til situasjon og legger increment på id

--- config/src/main/resources/db/migration/veilarbsituasjonDB/V1_19__oppretter_eksaleringsvarsel_tabell.sql
+++ config/src/main/resources/db/migration/veilarbsituasjonDB/V1_19__oppretter_eksaleringsvarsel_tabell.sql
@@ -1,10 +1,11 @@
 CREATE TABLE ESKALERINGSVARSEL (
-  varsel_id NUMBER,
+  varsel_id NUMBER GENERATED ALWAYS AS IDENTITY(START WITH 1 INCREMENT BY 1),
   aktorid NVARCHAR2(255),
   opprettet_av NVARCHAR2(255),
   opprettet_dato TIMESTAMP,
   avsluttet_dato TIMESTAMP,
   tilhorende_dialog_id NUMBER(19),
-  gjeldende NUMBER(1,0),
   PRIMARY KEY (varsel_id)
-)
+);
+
+ALTER TABLE SITUASJON ADD gjeldende_eskaleringsvarsel NUMBER;
