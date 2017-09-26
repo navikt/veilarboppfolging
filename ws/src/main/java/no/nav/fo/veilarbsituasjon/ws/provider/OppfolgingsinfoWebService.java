3c8666964439f979ab64d347716a8c806621001e
Hagen <H134988@ADEO.NO>
2017-09-26 14:47:54 +0200

    Lagt på metrikk på oppfølgingstjenesten.

--- ws/src/main/java/no/nav/fo/veilarbsituasjon/ws/provider/OppfolgingsinfoWebService.java
+++ ws/src/main/java/no/nav/fo/veilarbsituasjon/ws/provider/OppfolgingsinfoWebService.java
@@ -5,6 +5,8 @@ import no.nav.apiapp.soap.SoapTjeneste;
 import no.nav.fo.veilarbsituasjon.domain.AktorId;
 import no.nav.fo.veilarbsituasjon.domain.OppfolgingStatusData;
 import no.nav.fo.veilarbsituasjon.services.SituasjonOversiktService;
+import no.nav.metrics.MetricsFactory;
+import no.nav.metrics.Timer;
 import no.nav.tjeneste.virksomhet.oppfolgingsinfo.v1.OppfolgingsinfoV1;
 import no.nav.tjeneste.virksomhet.oppfolgingsinfo.v1.meldinger.OppfolgingsstatusRequest;
 import no.nav.tjeneste.virksomhet.oppfolgingsinfo.v1.meldinger.OppfolgingsstatusResponse;
@@ -21,17 +23,23 @@ public class OppfolgingsinfoWebService implements OppfolgingsinfoV1 {
 
     @Override
     public OppfolgingsstatusResponse hentOppfolgingsstatus(OppfolgingsstatusRequest request) {
+        final Timer timer = MetricsFactory.createTimer("ws.produsent." +
+                this.getClass().getSimpleName() + ".hentOppfolgingsstatus");
+        timer.start();
         OppfolgingStatusData oppfolgingStatusData = null;
         try {
             oppfolgingStatusData = situasjonOversiktService.hentOppfolgingsStatus(new AktorId(request.getAktorId()));
+            OppfolgingsstatusResponse response = new OppfolgingsstatusResponse();
+            response.setAktorId(request.getAktorId());
+            response.setErUnderOppfolging(oppfolgingStatusData.isUnderOppfolging());
+            response.setVeilederIdent(oppfolgingStatusData.getVeilederId());
+            return response;
         } catch (Exception e) {
             throw new RuntimeException(e);
+        } finally {
+            timer.stop();
+            timer.report();
         }
-        OppfolgingsstatusResponse response = new OppfolgingsstatusResponse();
-        response.setAktorId(request.getAktorId());
-        response.setErUnderOppfolging(oppfolgingStatusData.isUnderOppfolging());
-        response.setVeilederIdent(oppfolgingStatusData.getVeilederId());
-        return response;
     }
 
     @Override
