5d679624c104003e31d5192098caa73d11fcf5cd
Christian Finstad <christian.finstad@nav.no>
2017-10-03 07:17:23 +0200

    Fjerner ubrukt import

--- common/src/main/java/no/nav/fo/veilarbsituasjon/config/PepConfig.java
+++ common/src/main/java/no/nav/fo/veilarbsituasjon/config/PepConfig.java
@@ -3,7 +3,6 @@ package no.nav.fo.veilarbsituasjon.config;
 import no.nav.apiapp.security.PepClient;
 import no.nav.sbl.dialogarena.common.abac.pep.Pep;
 import no.nav.sbl.dialogarena.common.abac.pep.context.AbacContext;
-import no.nav.sbl.dialogarena.common.abac.pep.domain.ResourceType;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
