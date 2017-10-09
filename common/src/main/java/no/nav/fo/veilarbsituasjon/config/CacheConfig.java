12ff8e3b831ce3f141dda0456e7696828d7f71ce
Jan Erik Robertsen <jan.erik.robertsen@nav.no>
2017-10-09 13:12:14 +0200

    Legg til caching pa abac

--- common/src/main/java/no/nav/fo/veilarbsituasjon/config/CacheConfig.java
+++ common/src/main/java/no/nav/fo/veilarbsituasjon/config/CacheConfig.java
@@ -2,55 +2,30 @@ package no.nav.fo.veilarbsituasjon.config;
 
 import net.sf.ehcache.config.CacheConfiguration;
 import org.springframework.cache.CacheManager;
-import org.springframework.cache.annotation.CachingConfigurer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.ehcache.EhCacheCacheManager;
-import org.springframework.cache.interceptor.CacheErrorHandler;
-import org.springframework.cache.interceptor.CacheResolver;
-import org.springframework.cache.interceptor.KeyGenerator;
-import org.springframework.cache.interceptor.SimpleKeyGenerator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import static net.sf.ehcache.store.MemoryStoreEvictionPolicy.LRU;
+import static no.nav.sbl.dialogarena.common.abac.pep.context.AbacContext.ABAC_CACHE;
 
 @Configuration
 @EnableCaching
-public class CacheConfig implements CachingConfigurer {
+public class CacheConfig {
 
     public static final String HENT_ENHET = "hentEnhet";
 
+    private static final CacheConfiguration HENT_ENHET_CACHE = new CacheConfiguration(HENT_ENHET, 10000)
+            .memoryStoreEvictionPolicy(LRU)
+            .timeToIdleSeconds(3600)
+            .timeToLiveSeconds(3600);
+
     @Bean
-    public net.sf.ehcache.CacheManager ehCacheManager() {
-        net.sf.ehcache.config.Configuration config = new net.sf.ehcache.config.Configuration();
-        config.addCache(setupCache(HENT_ENHET));
-        return net.sf.ehcache.CacheManager.newInstance(config);
-    }
-
-    @Override
     public CacheManager cacheManager() {
-        return new EhCacheCacheManager(ehCacheManager());
-    }
-
-    @Override
-    public CacheResolver cacheResolver() {
-        return null;
-    }
-
-    @Override
-    public KeyGenerator keyGenerator() {
-        return new SimpleKeyGenerator();
-    }
-
-    @Override
-    public CacheErrorHandler errorHandler() {
-        return null;
-    }
-
-    private CacheConfiguration setupCache(String name) {
-        return new CacheConfiguration(name, 10000)
-                .memoryStoreEvictionPolicy(LRU)
-                .timeToIdleSeconds(3600)
-                .timeToLiveSeconds(3600);
+        net.sf.ehcache.config.Configuration config = new net.sf.ehcache.config.Configuration();
+        config.addCache(HENT_ENHET_CACHE);
+        config.addCache(ABAC_CACHE);
+        return new EhCacheCacheManager(net.sf.ehcache.CacheManager.newInstance(config));
     }
 }
