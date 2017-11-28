93bc79a204cf71809b001c1f0808b821b6184df1
Jan Berge Ommedal <jan.berge.ommedal@nav.no>
2017-11-28 20:04:36 +0100

    debug

--- common/src/test/java/no/nav/fo/veilarboppfolging/utils/DateUtilsTest.java
+++ common/src/test/java/no/nav/fo/veilarboppfolging/utils/DateUtilsTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import java.sql.Timestamp;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.util.TimeZone;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -22,7 +23,12 @@ public class DateUtilsTest {
     public void toZonedDateTime() throws Exception {
         Timestamp timestamp = Timestamp.valueOf("2017-04-19 12:21:04.963");
         String expectedString = "2017-04-19T12:21:04.963+02:00";
-        ZonedDateTime zoned = ZonedDateTime.of(timestamp.toLocalDateTime(), ZoneId.of("+02:00"));
+        ZoneId of = ZoneId.of("+02:00");
+        ZonedDateTime zoned = ZonedDateTime.of(timestamp.toLocalDateTime(), of);
+        System.out.println(zoned);
+        System.out.println(of);
+        System.out.println(System.getenv());
+        System.out.println(TimeZone.getDefault());
         assertEquals(expectedString, zoned.toString());
     }
 }
