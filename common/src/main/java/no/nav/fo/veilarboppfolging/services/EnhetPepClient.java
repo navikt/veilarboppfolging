abec7dc112efad37e7ae5fc033e0d9c96ce5f1bb
Jan Erik Robertsen <jan.erik.robertsen@nav.no>
2018-01-05 15:30:42 +0100

    FO-225: Use constant for pep domain

--- common/src/main/java/no/nav/fo/veilarboppfolging/services/EnhetPepClient.java
+++ common/src/main/java/no/nav/fo/veilarboppfolging/services/EnhetPepClient.java
@@ -5,12 +5,13 @@ import no.nav.apiapp.feil.IngenTilgang;
 import no.nav.sbl.dialogarena.common.abac.pep.Pep;
 import no.nav.sbl.dialogarena.common.abac.pep.domain.ResourceType;
 import no.nav.sbl.dialogarena.common.abac.pep.domain.response.BiasedDecisionResponse;
-import no.nav.sbl.dialogarena.common.abac.pep.domain.response.Decision;
 import org.springframework.stereotype.Component;
 
 import javax.inject.Inject;
 
 import static java.lang.String.format;
+import static no.nav.fo.veilarboppfolging.config.PepConfig.DOMAIN_VEILARB;
+import static no.nav.sbl.dialogarena.common.abac.pep.domain.response.Decision.Permit;
 
 @Component
 public class EnhetPepClient {
@@ -22,9 +23,9 @@ public class EnhetPepClient {
     public void sjekkTilgang(String enhet) {
         BiasedDecisionResponse response = pep.harTilgang(pep.nyRequest()
                 .withResourceType(ResourceType.Enhet)
-                .withDomain("veilarb")
+                .withDomain(DOMAIN_VEILARB)
                 .withEnhet(enhet));
-        if (response.getBiasedDecision() != Decision.Permit) {
+        if (response.getBiasedDecision() != Permit) {
             throw new IngenTilgang(format("Veileder har ikke tilgang til enhet '%s'", enhet));
         }
     }
