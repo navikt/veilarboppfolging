3ba9e1f7a117c6975a85dd5a28e3f84372c9e311
Joselito Poblete <joselito.poblete@gmail.com>
2018-01-31 16:22:06 +0100

    FO-230 Lag ny tabell for registrering

--- common/src/test/java/no/nav/fo/veilarboppfolging/db/ArbeidssokerregistreringRepositoryTest.java
+++ common/src/test/java/no/nav/fo/veilarboppfolging/db/ArbeidssokerregistreringRepositoryTest.java
@@ -1,10 +1,13 @@
 package no.nav.fo.veilarboppfolging.db;
 
 import no.nav.fo.veilarboppfolging.domain.AktorId;
+import no.nav.fo.veilarboppfolging.domain.BrukerRegistrering;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.core.JdbcTemplate;
 
+import java.sql.Timestamp;
+import static java.lang.System.currentTimeMillis;
 import static no.nav.fo.veilarboppfolging.config.JndiLocalContextConfig.setupInMemoryDatabase;
 import static org.assertj.core.api.Java6Assertions.assertThat;
 
@@ -38,5 +41,20 @@ public class ArbeidssokerregistreringRepositoryTest {
     public void oppfolgingsflaggSkalVaereFalseNaarIkkeSatt() {
         boolean oppfolgingsflagg = arbeidssokerregistreringRepository.erOppfolgingsflaggSatt(new AktorId("11111"));
         assertThat(oppfolgingsflagg).isFalse();
+    }
+
+    @Test
+    public void registrerBruker() {
+        BrukerRegistrering bruker = new BrukerRegistrering()
+                .setAktorId(new AktorId("11111").getAktorId())
+                .setOpprettetDato(new Timestamp(currentTimeMillis()))
+                .setEnigIOppsummering(true)
+                .setOppsummering("Test test oppsummering")
+                .setBesvarelse("{1:1, 2:1, 3:1, 4:1, 5:1}");
+        BrukerRegistrering registrertBruker = arbeidssokerregistreringRepository.registrerBruker(bruker);
+
+        assertThat(registrertBruker.isEnigIOppsummering()).isEqualTo(bruker.isEnigIOppsummering());
+        assertThat(registrertBruker.getOppsummering()).isEqualTo(bruker.getOppsummering());
+        assertThat(registrertBruker.getBesvarelse()).isEqualTo(bruker.getBesvarelse());
     }
 }
