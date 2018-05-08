d27597609e544c8aef59e2ef64cfd765e257dec9
Jan Berge Ommedal <jan.berge.ommedal@nav.no>
2018-05-08 23:15:12 +0200

    PUS-162 test av ny autentifiseringsmekanisme

--- smoketest/src/test/java/no/nav/fo/veilarboppfolging/ws/provider/OppfolgingsinfoSmoketest.java
+++ smoketest/src/test/java/no/nav/fo/veilarboppfolging/ws/provider/OppfolgingsinfoSmoketest.java
@@ -1,11 +1,14 @@
 package no.nav.fo.veilarboppfolging.ws.provider;
 
-import no.nav.brukerdialog.security.context.InternbrukerSubjectHandler;
+import no.nav.brukerdialog.security.domain.IdentType;
 import no.nav.brukerdialog.security.domain.OidcCredential;
-import no.nav.brukerdialog.security.oidc.TokenUtils;
 import no.nav.brukerdialog.security.oidc.UserTokenProvider;
+import no.nav.common.auth.SsoToken;
+import no.nav.common.auth.Subject;
+import no.nav.common.auth.SubjectHandler;
 import no.nav.dialogarena.config.DevelopmentSecurity;
 import no.nav.sbl.dialogarena.common.cxf.CXFClient;
+import no.nav.sbl.util.EnvironmentUtils;
 import no.nav.tjeneste.virksomhet.oppfolgingsinfo.v1.OppfolgingsinfoV1;
 import no.nav.tjeneste.virksomhet.oppfolgingsinfo.v1.meldinger.OppfolgingsstatusRequest;
 import org.apache.cxf.interceptor.LoggingOutInterceptor;
@@ -16,8 +19,6 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Objects;
 
-import static java.lang.System.getProperty;
-import static java.lang.System.setProperty;
 import static no.nav.dialogarena.config.DevelopmentSecurity.setupIntegrationTestSecurity;
 import static no.nav.sbl.dialogarena.test.junit.Tag.SMOKETEST;
 
@@ -25,36 +26,34 @@ import static no.nav.sbl.dialogarena.test.junit.Tag.SMOKETEST;
 public class OppfolgingsinfoSmoketest {
 
     private static final String AREMARK_AKTORID = "***REMOVED***42";
-    public static final String SMOKETEST = "smoketest";
     private OppfolgingsinfoV1 oppfolgingsinfoV1;
     private static String hostname;
     private static String MILJO;
 
     @BeforeAll
     public static void setup() {
-        MILJO = getProperty("miljo");
+        MILJO = EnvironmentUtils.getOptionalProperty("miljo").orElse(null);
         setupIntegrationTestSecurity(new DevelopmentSecurity.IntegrationTestConfig("veilarboppfolging"));
-        setProperty("no.nav.brukerdialog.security.context.subjectHandlerImplementationClass", InternbrukerSubjectHandler.class.getName());
     }
 
     @BeforeEach
     public void before() throws Exception {
-        hostname = Objects.nonNull(MILJO) ? String.format("https://app-%s.adeo.no/", MILJO) : "http://localhost:8080/";
+        hostname = Objects.nonNull(MILJO) ? String.format("https://app-%s.adeo.no/", MILJO) : "https://localhost:8080/";
         oppfolgingsinfoV1 = new CXFClient<>(OppfolgingsinfoV1.class)
                 .withOutInterceptor(new LoggingOutInterceptor())
                 .address(hostname + "veilarboppfolging-ws/ws/oppfolgingsinfo")
                 .configureStsForOnBehalfOfWithJWT()
                 .build();
-
-        UserTokenProvider userTokenProvider = new UserTokenProvider();
-        OidcCredential token = userTokenProvider.getIdToken();
-        InternbrukerSubjectHandler.setOidcCredential(token);
-        InternbrukerSubjectHandler.setVeilederIdent(TokenUtils.getTokenSub(token.getToken()));
     }
 
     @Test
     public void hentOppfolgingsinfo() {
-        OppfolgingsstatusRequest request = new OppfolgingsstatusRequest().withAktorId(AREMARK_AKTORID);
-        oppfolgingsinfoV1.hentOppfolgingsstatus(request);
+        UserTokenProvider userTokenProvider = new UserTokenProvider();
+        OidcCredential token = userTokenProvider.getIdToken();
+        Subject subject = new Subject("uid", IdentType.InternBruker, SsoToken.oidcToken(token.getToken()));
+        SubjectHandler.withSubject(subject, () -> {
+            OppfolgingsstatusRequest request = new OppfolgingsstatusRequest().withAktorId(AREMARK_AKTORID);
+            oppfolgingsinfoV1.hentOppfolgingsstatus(request);
+        });
     }
 }
