a10f7988a94b9db788f6f496f498690ab26cb596
Jan Berge Ommedal <jan.berge.ommedal@nav.no>
2018-05-08 23:15:12 +0200

    PUS-162 test av ny autentifiseringsmekanisme

--- ws/src/main/java/no/nav/fo/veilarboppfolging/config/OrganisasjonsenhetWSConfig.java
+++ ws/src/main/java/no/nav/fo/veilarboppfolging/config/OrganisasjonsenhetWSConfig.java
@@ -1,6 +1,6 @@
 package no.nav.fo.veilarboppfolging.config;
 
-import no.nav.modig.security.ws.UserSAMLOutInterceptor;
+import no.nav.sbl.dialogarena.common.cxf.SamlPropagatingOutInterceptor;
 import no.nav.tjeneste.virksomhet.organisasjonenhet.v1.OrganisasjonEnhetV1;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,7 +11,7 @@ public class OrganisasjonsenhetWSConfig {
     @Bean
     public OrganisasjonEnhetV1 organisasjonEnhetPortType() {
         return OrganisasjonsenhetConfig.organisasjonEnhetPortType()
-                .withOutInterceptor(new UserSAMLOutInterceptor())
+                .withOutInterceptor(new SamlPropagatingOutInterceptor())
                 .build();
     }
 
