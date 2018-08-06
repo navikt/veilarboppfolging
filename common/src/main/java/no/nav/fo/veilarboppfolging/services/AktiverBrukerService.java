60d7c4f07db1aa4b9a610d568c8a64003d45073e
Ola <ola.galde@nav.no>
2018-08-06 11:48:09 +0200

    FO-1303: Fjerner gammel, ubrukt kode.

--- common/src/main/java/no/nav/fo/veilarboppfolging/services/AktiverBrukerService.java
+++ common/src/main/java/no/nav/fo/veilarboppfolging/services/AktiverBrukerService.java
@@ -29,7 +29,6 @@ import static io.vavr.API.Case;
 import static io.vavr.Predicates.instanceOf;
 import static java.util.Optional.ofNullable;
 import static javax.ws.rs.core.Response.Status.INTERNAL_SERVER_ERROR;
-import static no.nav.fo.veilarboppfolging.domain.Innsatsgruppe.STANDARD_INNSATS;
 
 @Slf4j
 @Component
@@ -77,7 +76,7 @@ public class AktiverBrukerService {
 
         AktorId aktorId = FnrUtils.getAktorIdOrElseThrow(aktorService, fnr);
 
-        aktiverBrukerOgOppfolging(fnr, aktorId, hentInnsatsGruppeBakoverKompatibel(bruker));
+        aktiverBrukerOgOppfolging(fnr, aktorId, bruker.getInnsatsgruppe());
     }
 
     @Transactional
@@ -104,12 +103,6 @@ public class AktiverBrukerService {
         if (opprettBrukerIArenaFeature.erAktiv()) {
             reaktiverBrukerIArena(fnr);
         }
-    }
-
-
-    //Todo: fjerne denne metoden når FO-1123 går over i Test1.
-    private Innsatsgruppe hentInnsatsGruppeBakoverKompatibel(AktiverArbeidssokerData aktiverArbeidssokerData) {
-        return ofNullable(aktiverArbeidssokerData.getInnsatsgruppe()).orElse(STANDARD_INNSATS);
     }
 
     private void aktiverBrukerOgOppfolging(String fnr, AktorId aktorId, Innsatsgruppe innsatsgruppe) {
