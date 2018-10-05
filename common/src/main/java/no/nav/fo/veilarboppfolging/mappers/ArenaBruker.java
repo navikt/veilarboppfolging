f1a35afd91bceaa52f6da9265fcf5d853be134af
Sneha <desaisneha.2011@gmail.com>
2018-10-05 14:23:44 +0200

    [FO-1306] Fjerner ubrukt kode

--- common/src/main/java/no/nav/fo/veilarboppfolging/mappers/ArenaBruker.java
+++ common/src/main/java/no/nav/fo/veilarboppfolging/mappers/ArenaBruker.java
@@ -11,18 +11,4 @@ public class ArenaBruker {
     public String aktoerid;
     public String formidlingsgruppekode;
     public ZonedDateTime iserv_fra_dato;
-    public String fodselsnr;
-    public String etternavn;
-    public String fornavn;
-    public String nav_kontor;
-    public String kvalifiseringsgruppekode;
-    public String rettighetsgruppekode;
-    public String hovedmaalkode;
-    public String sikkerhetstiltak_type_kode;
-    public String fr_kode;
-    public Boolean har_oppfolgingssak;
-    public String sperret_ansatt;
-    public Boolean er_doed;
-    public ZonedDateTime doed_fra_dato;
-    public ZonedDateTime endret_dato;
 }
