6b0fb6cb526140cdfa0e42fdc0f751ea2118c5f4
Sneha <desaisneha.2011@gmail.com>
2018-10-05 12:45:34 +0200

    [FO-1306] Legger metrikker for kafka consumer

--- common/src/main/java/no/nav/fo/veilarboppfolging/kafka/Consumer.java
+++ common/src/main/java/no/nav/fo/veilarboppfolging/kafka/Consumer.java
@@ -2,13 +2,12 @@ package no.nav.fo.veilarboppfolging.kafka;
 
 import no.nav.fo.veilarboppfolging.mappers.ArenaBruker;
 import no.nav.fo.veilarboppfolging.services.Iserv28Service;
+import no.nav.fo.veilarboppfolging.utils.FunksjonelleMetrikker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
-
 import javax.inject.Inject;
-
 import static no.nav.json.JsonUtils.fromJson;
 
 @Component
@@ -30,6 +29,7 @@ public class Consumer {
         try {
             final ArenaBruker deserialisertBruker = deserialisereBruker(arenaBruker);
             iserv28Service.filterereIservBrukere(deserialisertBruker);
+            FunksjonelleMetrikker.antallMeldingerKonsumertAvKafka();
             LOG.info("endret bruker= '{}'", deserialisertBruker);
         } catch (Throwable t) {
             LOG.error(t.getMessage(), t);
