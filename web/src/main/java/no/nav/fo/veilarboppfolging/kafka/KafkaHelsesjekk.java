f06c8f011eae5afd50be6672d13d92449d06b484
Peder Korsveien <peder.korsveien@nav.no>
2018-11-27 11:21:44 +0100

    KAIZEN-0 Endre kafkaa helsesjekk

--- web/src/main/java/no/nav/fo/veilarboppfolging/kafka/KafkaHelsesjekk.java
+++ web/src/main/java/no/nav/fo/veilarboppfolging/kafka/KafkaHelsesjekk.java
@@ -3,26 +3,29 @@ package no.nav.fo.veilarboppfolging.kafka;
 import lombok.extern.slf4j.Slf4j;
 import no.nav.apiapp.selftest.Helsesjekk;
 import no.nav.apiapp.selftest.HelsesjekkMetadata;
-import org.springframework.context.ApplicationListener;
-import org.springframework.kafka.event.NonResponsiveConsumerEvent;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.listener.ContainerAwareErrorHandler;
+import org.springframework.kafka.listener.MessageListenerContainer;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
+import java.util.List;
 
 import static no.nav.fo.veilarboppfolging.kafka.ConsumerConfig.getBrokerUrls;
 
 @Component
 @Slf4j
-public class KafkaHelsesjekk implements Helsesjekk, ApplicationListener<NonResponsiveConsumerEvent> {
+public class KafkaHelsesjekk implements Helsesjekk, ContainerAwareErrorHandler {
 
-    private long lastNonResponsiveEvent;
+    private long lastThrownExceptionTime;
+    private Exception e;
 
     @Override
     public void helsesjekk() throws Throwable {
-        // kafka rapporterer om ikke-responsiv klient hvert 30. sekund.
-        // feiler selftest når det er mindre enn 60 sekunder siden forrige NonResponsiveConsumerEvent
-        if ((lastNonResponsiveEvent + 60_000L) > System.currentTimeMillis()) {
-            throw new IllegalArgumentException("Kafka consumer var ikke-responsiv " + new Date(lastNonResponsiveEvent));
+        // hvis en exception har blitt kastet innen det siste minuttet antar vi det er noe feil med spring-klienten
+        if ((lastThrownExceptionTime + 60_000L) > System.currentTimeMillis()) {
+            throw new IllegalArgumentException("Kafka consumer feilet " + new Date(lastThrownExceptionTime), e);
         }
     }
 
@@ -32,8 +35,10 @@ public class KafkaHelsesjekk implements Helsesjekk, ApplicationListener<NonRespo
     }
 
     @Override
-    public void onApplicationEvent(NonResponsiveConsumerEvent nonResponsiveConsumerEvent) {
-        log.warn(nonResponsiveConsumerEvent.toString());
-        lastNonResponsiveEvent = System.currentTimeMillis();
+    public void handle(Exception thrownException, List<ConsumerRecord<?, ?>> records, Consumer<?, ?> consumer, MessageListenerContainer container) {
+        log.error("Feil i listener:", thrownException);
+        lastThrownExceptionTime = System.currentTimeMillis();
+        e = thrownException;
     }
+
 }
