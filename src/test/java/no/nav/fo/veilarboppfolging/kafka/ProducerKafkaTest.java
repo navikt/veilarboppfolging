692b83c39cf794358c186f947913977bbda1a9c7
Alexandra Back <alexandra.back2@nav.no>
2019-06-19 10:49:46 +0200

    FO-2275: Kafka-produsent tester

--- src/test/java/no/nav/fo/veilarboppfolging/kafka/ProducerKafkaTest.java
+++ src/test/java/no/nav/fo/veilarboppfolging/kafka/ProducerKafkaTest.java
@@ -16,27 +16,47 @@ public class ProducerKafkaTest extends KafkaTest {
 
     private AvsluttOppfolgingEndringRepository avsluttOppfolgingEndringRepository;
     private JdbcTemplate db;
+    private AvsluttOppfolgingProducer avsluttOppfolgingProducer;
 
     @Before
     public void setup() {
         db = new JdbcTemplate(setupInMemoryDatabase());
         avsluttOppfolgingEndringRepository = new AvsluttOppfolgingEndringRepository(db);
-        avsluttOppfolgingEndringRepository.insertAvsluttOppfolgingBruker("1234");
+        avsluttOppfolgingProducer = new AvsluttOppfolgingProducer(kafkaTemplate, avsluttOppfolgingEndringRepository, "HELLO_WORLD_TOPIC");
     }
 
-
     @Test
-    public void skallSletteAvsluttBrukerVedVedlykkedSendning() {
-        AvsluttOppfolgingProducer avsluttOppfolgingProducer  = new AvsluttOppfolgingProducer(kafkaTemplate, avsluttOppfolgingEndringRepository, "HELLO_WORLD_TOPIC");
+    public void slett_avsluttbruker_fra_db_ved_vedlykkedSendning() {
+        avsluttOppfolgingEndringRepository.insertAvsluttOppfolgingBruker("1234");
+        List<AvsluttOppfolgingKafkaDTO> alleBrukere = avsluttOppfolgingEndringRepository.hentAvsluttOppfolgingBrukere();
+        assertThat(alleBrukere.size()).isEqualTo(1);
         avsluttOppfolgingProducer.avsluttOppfolgingEvent("1234", new Date());
 
         boolean prosessert = false;
         while(!prosessert) {
             try {
                 Thread.sleep(10);
-                List<AvsluttOppfolgingKafkaDTO> alleBrukere = avsluttOppfolgingEndringRepository.hentAvsluttOppfolgingBrukere();
-                assertThat(alleBrukere.size()).isEqualTo(0);
                 prosessert = true;
+                alleBrukere = avsluttOppfolgingEndringRepository.hentAvsluttOppfolgingBrukere();
+                assertThat(alleBrukere.size()).isEqualTo(0);
+            } catch(Throwable a) {
+            }
+        }
+    }
+
+    @Test
+    public void gitt_att_misslyckades_produsere_kafka_meldig_skall_ikke_slette_nyare_inslag_i_db() {
+        avsluttOppfolgingEndringRepository.insertAvsluttOppfolgingBruker("1234");
+        long DAG_I_MILLISEK = 1000 * 60 * 60 * 24;
+        avsluttOppfolgingProducer.avsluttOppfolgingEvent("1234", new Date(System.currentTimeMillis() - (7 * DAG_I_MILLISEK)));
+
+        boolean prosessert = false;
+        while(!prosessert) {
+            try {
+                Thread.sleep(10);
+                prosessert = true;
+                List<AvsluttOppfolgingKafkaDTO> alleBrukere = avsluttOppfolgingEndringRepository.hentAvsluttOppfolgingBrukere();
+                assertThat(alleBrukere.size()).isEqualTo(1);
             } catch(Throwable a) {
             }
         }
