4ef69afe176d905bc5e0dcc29cbd61143a7619a2
Alexandra Back <alexandra.back2@nav.no>
2019-06-19 11:27:20 +0200

    FO-2275: Øke tiden scheduleren kjøres. Leg til logging av brukere som skall sendes på kafka vid schedulering

--- src/main/java/no/nav/fo/veilarboppfolging/schedule/AvsluttOppfolgingKafkaFeilSchedule.java
+++ src/main/java/no/nav/fo/veilarboppfolging/schedule/AvsluttOppfolgingKafkaFeilSchedule.java
@@ -1,5 +1,6 @@
 package no.nav.fo.veilarboppfolging.schedule;
 
+import lombok.extern.slf4j.Slf4j;
 import no.nav.fo.veilarboppfolging.db.AvsluttOppfolgingEndringRepository;
 import no.nav.fo.veilarboppfolging.domain.AvsluttOppfolgingKafkaDTO;
 import no.nav.fo.veilarboppfolging.kafka.AvsluttOppfolgingProducer;
@@ -9,10 +10,11 @@ import org.springframework.stereotype.Component;
 import javax.inject.Inject;
 import java.util.List;
 
+@Slf4j
 @Component
 public class AvsluttOppfolgingKafkaFeilSchedule {
 
-    public static final long SCHEDULE_DELAY = 60 * 10 * 1000; // 15 minutes
+    public static final long SCHEDULE_DELAY = 60 * 60 * 1000; // 60 minutes
 
     private AvsluttOppfolgingEndringRepository avsluttOppfolgingEndringRepository;
 
@@ -24,10 +26,11 @@ public class AvsluttOppfolgingKafkaFeilSchedule {
         this.avsluttOppfolgingProducer = avsluttOppfolgingProducer;
     }
 
-    @Scheduled(fixedDelay = SCHEDULE_DELAY, initialDelay = 60 * 1000)
+    @Scheduled(fixedDelay = SCHEDULE_DELAY, initialDelay = 15 * 60 * 1000)
     public void sendFeiledeKafkaMeldinger() {
-        List<AvsluttOppfolgingKafkaDTO> feiledeMeldinger = avsluttOppfolgingEndringRepository.hentAvsluttOppfolgingBrukere();
-        feiledeMeldinger.forEach(feiletMelding -> avsluttOppfolgingProducer.avsluttOppfolgingEvent(feiletMelding.getAktorId(), feiletMelding.getSluttdato()));
+        List<AvsluttOppfolgingKafkaDTO> avsluttOppfolgingKafkaBrukere = avsluttOppfolgingEndringRepository.hentAvsluttOppfolgingBrukere();
+        log.info("Starter jobb for legge til avslutning av brukere {} på kafka", avsluttOppfolgingKafkaBrukere);
+        avsluttOppfolgingKafkaBrukere.forEach(feiletMelding -> avsluttOppfolgingProducer.avsluttOppfolgingEvent(feiletMelding.getAktorId(), feiletMelding.getSluttdato()));
     }
 
 }
