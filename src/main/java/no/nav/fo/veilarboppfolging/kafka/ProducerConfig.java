e874aeaee6933f20d7a1f347b4d3649ea5356dd3
Peder Korsveien <peder.korsveien@nav.no>
2019-11-01 16:46:13 +0100

    KZ-0 Hent ut APP_ENVIRONMENT_NAME

--- src/main/java/no/nav/fo/veilarboppfolging/kafka/ProducerConfig.java
+++ src/main/java/no/nav/fo/veilarboppfolging/kafka/ProducerConfig.java
@@ -8,12 +8,13 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.support.LoggingProducerListener;
 
+import static no.nav.sbl.util.EnvironmentUtils.getRequiredProperty;
 import static no.nav.sbl.util.EnvironmentUtils.requireEnvironmentName;
 
 @Configuration
 public class ProducerConfig {
 
-    private static final String KAFKA_PRODUCER_TOPIC = "aapen-fo-endringPaaAvsluttOppfolging-v1" + "-" + requireEnvironmentName();
+    private static final String KAFKA_PRODUCER_TOPIC = "aapen-fo-endringPaaAvsluttOppfolging-v1" + "-" + getRequiredProperty("APP_ENVIRONMENT_NAME");
 
     static ProducerFactory<String, String> producerFactory() {
         return new DefaultKafkaProducerFactory<>(KafkaPropsConfig.kafkaProducerProperties());
