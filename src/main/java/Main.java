b1a77d66401d50197b6430b9b0669379db14bddd
Peder Korsveien <peder.korsveien@nav.no>
2019-11-06 06:53:46 +0100

    KZ-0 Oppgrader common

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,8 @@
 import no.nav.apiapp.ApiApp;
 
 import no.nav.brukerdialog.tools.SecurityConstants;
+import no.nav.common.utils.NaisUtils;
 import no.nav.fo.veilarboppfolging.config.ApplicationConfig;
-import no.nav.fo.veilarboppfolging.utils.NaiseratorUtils;
 import no.nav.sbl.dialogarena.common.abac.pep.CredentialConstants;
 import no.nav.sbl.dialogarena.common.cxf.StsSecurityConstants;
 
@@ -12,7 +12,6 @@ import static no.nav.dialogarena.aktor.AktorConfig.AKTOER_ENDPOINT_URL;
 import static no.nav.fo.veilarboppfolging.config.ApplicationConfig.*;
 import static no.nav.fo.veilarboppfolging.config.DatabaseConfig.VEILARBOPPFOLGINGDB_PASSWORD_PROPERTY;
 import static no.nav.fo.veilarboppfolging.config.DatabaseConfig.VEILARBOPPFOLGINGDB_USERNAME_PROPERTY;
-import static no.nav.fo.veilarboppfolging.utils.NaiseratorUtils.getDefaultSecretPath;
 import static no.nav.sbl.util.EnvironmentUtils.getRequiredProperty;
 
 public class Main {
@@ -21,7 +20,7 @@ public class Main {
         setProperty(AKTOER_ENDPOINT_URL, getRequiredProperty(AKTOER_V2_URL_PROPERTY));
         setProperty(OIDC_REDIRECT_URL_PROPERTY_NAME, getRequiredProperty(VEILARBLOGIN_REDIRECT_URL_URL_PROPERTY));
 
-        NaiseratorUtils.Credentials serviceUser = NaiseratorUtils.getCredentials(getDefaultSecretPath("service_user"));
+        NaisUtils.Credentials serviceUser = NaisUtils.getCredentials("service_user");
 
         //ABAC
         System.setProperty(CredentialConstants.SYSTEMUSER_USERNAME, serviceUser.username);
@@ -36,8 +35,7 @@ public class Main {
         System.setProperty(SecurityConstants.SYSTEMUSER_PASSWORD, serviceUser.password);
 
 
-
-        NaiseratorUtils.Credentials oracleCreds = NaiseratorUtils.getCredentials(getDefaultSecretPath("oracle_creds"));
+        NaisUtils.Credentials oracleCreds = NaisUtils.getCredentials("oracle_creds");
         System.setProperty(VEILARBOPPFOLGINGDB_USERNAME_PROPERTY, oracleCreds.username);
         System.setProperty(VEILARBOPPFOLGINGDB_PASSWORD_PROPERTY, oracleCreds.password);
 
