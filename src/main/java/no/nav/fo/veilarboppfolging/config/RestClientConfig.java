96d4459e98bb39980f76bf4ae6804178a3b788cd
Alexandra Back <alexandra.back2@nav.no>
2019-12-02 17:26:20 +0100

    KAIZEN-0: Fjern dkif-soap. Legg til JSONException på dkif-responsen

--- src/main/java/no/nav/fo/veilarboppfolging/config/RestClientConfig.java
+++ src/main/java/no/nav/fo/veilarboppfolging/config/RestClientConfig.java
@@ -1,5 +1,6 @@
 package no.nav.fo.veilarboppfolging.config;
 
+import no.nav.fo.veilarboppfolging.services.DkifService;
 import no.nav.fo.veilarboppfolging.services.VeilarbaktivtetService;
 import no.nav.sbl.dialogarena.restclient.RestClient;
 import no.nav.sbl.util.EnvironmentUtils;
@@ -17,6 +18,11 @@ public class RestClientConfig {
     @Bean
     public VeilarbaktivtetService veilarbaktivtetService(Provider<HttpServletRequest> httpServletRequestProvider) {
         return new VeilarbaktivtetService(RestClient.build(httpServletRequestProvider, EnvironmentUtils.getRequiredProperty(VEILARBAKTIVITETAPI_URL_PROPERTY)));
+    }
+
+    @Bean
+    public DkifService dkifService() {
+        return new DkifService();
     }
 
 }
