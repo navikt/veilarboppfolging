5d1251f376493f37df98ab16baa0eb41b67a90b6
Peder Korsveien <peder.korsveien@nav.no>
2020-05-01 14:51:01 +0200

    FO-2703 Konfigurer kafka for max throughput

--- src/main/java/no/nav/fo/veilarboppfolging/kafka/ProducerConfig.java
+++ src/main/java/no/nav/fo/veilarboppfolging/kafka/ProducerConfig.java
@@ -48,9 +48,10 @@ public class ProducerConfig {
     public OppfolgingKafkaProducer oppfolgingStatusProducer(OppfolgingFeedRepository repository, OppfolgingKafkaFeiletMeldingRepository feiletMeldingRepository,  AktorService aktorService) {
         HashMap<String, Object> config = kafkaProducerProperties();
 
-        config.put(ACKS_CONFIG, "1");                  // Leader will now wait for ACKs from all followers
-        config.put(BATCH_SIZE_CONFIG, 1024*300);     // 300KiB batch size
-        config.put(MAX_BLOCK_MS_CONFIG, 60*10_000);  // 10s timeout
+        config.put(ACKS_CONFIG, "0");                  // Fire-and-forget, we do not care about acks when hydrating
+        config.put(BATCH_SIZE_CONFIG, 25*1024*1000);   // 25MiB Buffer
+        config.put(MAX_BLOCK_MS_CONFIG, 60*20*1000);   // 20s timeout when buffer is full or requesting metadata for topic
+        config.put(REQUEST_TIMEOUT_MS_CONFIG, 1000);   // 1s timeout for waiting on reply from server
 
         val kafkaProducer = new KafkaProducer<String, String>(config);
         return new OppfolgingKafkaProducer(kafkaProducer, repository, feiletMeldingRepository, aktorService, KAFKA_PRODUCER_TOPIC_OPPFOLGING);
