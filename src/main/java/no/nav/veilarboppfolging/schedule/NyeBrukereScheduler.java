0c1fe3d4c00da64b5a1d6f50c0cd882996ef4100
Alexander Gård <alex.gaard@hotmail.com>
2020-08-14 10:51:54 +0200

    FO-2944 Wrap NyeBrukereScheduler med isLeader sjekk

--- src/main/java/no/nav/veilarboppfolging/schedule/NyeBrukereScheduler.java
+++ src/main/java/no/nav/veilarboppfolging/schedule/NyeBrukereScheduler.java
@@ -1,5 +1,6 @@
 package no.nav.veilarboppfolging.schedule;
 
+import no.nav.common.leaderelection.LeaderElectionClient;
 import no.nav.veilarboppfolging.repository.NyeBrukereFeedRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -9,15 +10,20 @@ import org.springframework.stereotype.Component;
 @Component
 public class NyeBrukereScheduler {
 
+    private final LeaderElectionClient leaderElectionClient;
+
     private final NyeBrukereFeedRepository nyeBrukereFeedRepository;
 
     @Autowired
-    public NyeBrukereScheduler(NyeBrukereFeedRepository nyeBrukereFeedRepository) {
+    public NyeBrukereScheduler(LeaderElectionClient leaderElectionClient, NyeBrukereFeedRepository nyeBrukereFeedRepository) {
+        this.leaderElectionClient = leaderElectionClient;
         this.nyeBrukereFeedRepository = nyeBrukereFeedRepository;
     }
 
     @Scheduled(fixedDelayString = "${nyebrukere.oppdater.id.rate:10000}")
     public void settIdeerPaFeedElementer() {
-        nyeBrukereFeedRepository.leggTilFeedIdPaAlleElementerUtenFeedId();
+        if (leaderElectionClient.isLeader()) {
+            nyeBrukereFeedRepository.leggTilFeedIdPaAlleElementerUtenFeedId();
+        }
     }
 }
