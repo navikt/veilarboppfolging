d120e0637196f81e8817fbecd1e5dc74de7ded09
Alexander Gård <alex.gaard@hotmail.com>
2020-10-16 14:46:00 +0200

    Bruk innlogget bruker sitt token for kall mot veilarbaktivitet

--- src/main/java/no/nav/veilarboppfolging/config/ClientConfig.java
+++ src/main/java/no/nav/veilarboppfolging/config/ClientConfig.java
@@ -7,7 +7,6 @@ import no.nav.common.client.norg2.CachedNorg2Client;
 import no.nav.common.client.norg2.Norg2Client;
 import no.nav.common.client.norg2.NorgHttp2Client;
 import no.nav.common.cxf.StsConfig;
-import no.nav.common.sts.OpenAmSystemUserTokenProvider;
 import no.nav.common.sts.SystemUserTokenProvider;
 import no.nav.veilarboppfolging.client.behandle_arbeidssoker.BehandleArbeidssokerClient;
 import no.nav.veilarboppfolging.client.behandle_arbeidssoker.BehandleArbeidssokerClientImpl;
@@ -62,9 +61,9 @@ public class ClientConfig {
     }
 
     @Bean
-    public VeilarbaktivitetClient veilarbaktivitetClient(OpenAmSystemUserTokenProvider openAmSystemUserTokenProvider) {
+    public VeilarbaktivitetClient veilarbaktivitetClient(AuthService authService) {
         String url = clusterUrlForApplication("veilarbaktivitet", true);
-        return new VeilarbaktivitetClientImpl(url, openAmSystemUserTokenProvider);
+        return new VeilarbaktivitetClientImpl(url, authService::getInnloggetBrukerToken);
     }
 
     @Bean
