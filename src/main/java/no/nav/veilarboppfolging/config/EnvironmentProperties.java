ca56397268b715a025475016453a73ee4c1a1da4
Alexander Gård <alex.gaard@hotmail.com>
2020-10-21 14:33:18 +0200

    La til støtte for validering av nye tokens fra loginservice idporten

--- src/main/java/no/nav/veilarboppfolging/config/EnvironmentProperties.java
+++ src/main/java/no/nav/veilarboppfolging/config/EnvironmentProperties.java
@@ -27,9 +27,9 @@ public class EnvironmentProperties {
     private String veilarbloginAadClientId;
 
 
-    private String aadB2cDiscoveryUrl;
+    private String loginserviceIdportenAudience;
 
-    private String aadB2cClientId;
+    private String loginserviceIdportenDiscoveryUrl;
 
 
     private String naisStsDiscoveryUrl;
