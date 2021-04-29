e22c37111c212ebf37ee683ea1dd15c863e93660
Cato Olsen <cato.olsen@nav.no>
2021-04-29 14:28:15 +0200

    Henter JDBC-config via environment.

--- src/main/java/no/nav/veilarboppfolging/config/DatabaseConfig.java
+++ src/main/java/no/nav/veilarboppfolging/config/DatabaseConfig.java
@@ -2,7 +2,11 @@ package no.nav.veilarboppfolging.config;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
-import no.nav.common.utils.Credentials;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -10,27 +14,20 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 
-import static no.nav.common.utils.NaisUtils.getCredentials;
-import static no.nav.common.utils.NaisUtils.getFileContent;
-
 @Configuration
 @EnableTransactionManagement
+@EnableConfigurationProperties(DatabaseConfig.DatasourceProperties.class)
+@RequiredArgsConstructor
 public class DatabaseConfig {
 
-    private final Credentials oracleCredentials;
-    private final String oracleURL;
-
-    public DatabaseConfig() {
-        oracleCredentials = getCredentials("oracle_creds");
-        oracleURL = getFileContent("/var/run/secrets/nais.io/oracle_config/jdbc_url");
-    }
+    private final DatasourceProperties datasourceProperties;
 
     @Bean
     public DataSource dataSource() {
-        HikariConfig config = new HikariConfig();
-        config.setJdbcUrl(oracleURL);
-        config.setUsername(oracleCredentials.username);
-        config.setPassword(oracleCredentials.password);
+        var config = new HikariConfig();
+        config.setJdbcUrl(datasourceProperties.url);
+        config.setUsername(datasourceProperties.username);
+        config.setPassword(datasourceProperties.password);
         config.setMaximumPoolSize(5);
 
         return new HikariDataSource(config);
@@ -39,6 +36,15 @@ public class DatabaseConfig {
     @Bean
     public JdbcTemplate db(DataSource dataSource) {
         return new JdbcTemplate(dataSource);
+    }
+
+    @Getter
+    @Setter
+    @ConfigurationProperties(prefix = "app.datasource")
+    public static class DatasourceProperties {
+        String url;
+        String username;
+        String password;
     }
 
 }
