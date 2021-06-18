c16c9ac1be823a96033a4b69c3d7375301e273f0
Alexander Gård <alex.gaard@hotmail.com>
2021-06-18 08:09:08 +0200

    Fikset dkif test

--- src/test/java/no/nav/veilarboppfolging/test/TestData.java
+++ src/test/java/no/nav/veilarboppfolging/test/TestData.java
@@ -5,12 +5,10 @@ import no.nav.common.types.identer.Fnr;
 
 public class TestData {
 
-    public final static Fnr TEST_FNR = Fnr.of("testFnr");
+    public final static Fnr TEST_FNR = Fnr.of("12345678900");
 
-    public final static AktorId TEST_AKTOR_ID = AktorId.of("testAktorId");
+    public final static AktorId TEST_AKTOR_ID = AktorId.of("11122233334445");
 
-    public final static AktorId TEST_AKTOR_ID_2 = AktorId.of("testAktorId2");
-
-    public final static String TEST_VEILEDER_IDENT = "Z12345";
+    public final static AktorId TEST_AKTOR_ID_2 = AktorId.of("5556667778889");
 
 }
