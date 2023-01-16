e2f00a636481a657d96aca07046563ed5c4b6c64
sigurdgroneng <sigurd.groneng@nav.no>
2023-01-16 12:32:35 +0100

    Remove unused filter

--- src/test/java/no/nav/veilarboppfolging/controller/v2/OppfolgingV2ControllerTest.java
+++ src/test/java/no/nav/veilarboppfolging/controller/v2/OppfolgingV2ControllerTest.java
@@ -34,7 +34,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(controllers = OppfolgingV2Controller.class, includeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION))
+@WebMvcTest(controllers = OppfolgingV2Controller.class)
 class OppfolgingV2ControllerTest {
 
     @Autowired
