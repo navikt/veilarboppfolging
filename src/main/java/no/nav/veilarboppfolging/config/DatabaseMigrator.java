8d1c3710699659dfc95fd77e032144b66f3273f6
Hans Petter Simonsen <hans.petter.simonsen@nav.no>
2023-03-21 15:57:52 +0100

    Legg til toggle for flyway repair

--- src/main/java/no/nav/veilarboppfolging/config/DatabaseMigrator.java
+++ src/main/java/no/nav/veilarboppfolging/config/DatabaseMigrator.java
@@ -1,23 +1,25 @@
 package no.nav.veilarboppfolging.config;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import no.nav.common.featuretoggle.UnleashClient;
 import org.flywaydb.core.Flyway;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import jakarta.annotation.PostConstruct;
 import javax.sql.DataSource;
+import java.util.List;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class DatabaseMigrator {
 
-    private final DataSource dataSource;
+    public static final String FLYWAY_REPAIR_TOGGLE = "veilarboppfolging.flyway.repair";
 
-    @Autowired
-    public DatabaseMigrator(DataSource dataSource) {
-        this.dataSource = dataSource;
-    }
+    private final DataSource dataSource;
+    private final UnleashClient unleashClient;
+
 
     @PostConstruct
     public void migrateDb() {
@@ -25,6 +27,12 @@ public class DatabaseMigrator {
         var flyway = new Flyway(Flyway.configure()
                 .dataSource(dataSource)
                 .table("schema_version"));
+        if (unleashClient.isEnabled(FLYWAY_REPAIR_TOGGLE)) {
+            List<String> warnings = flyway.info().getInfoResult().warnings;
+            log.warn("Flyway warnings: {}", warnings);
+            log.warn("Toggle for flyway repair aktiv. Kjører flyway repair før migrate");
+            flyway.repair();
+        }
         flyway.migrate();
     }
 
