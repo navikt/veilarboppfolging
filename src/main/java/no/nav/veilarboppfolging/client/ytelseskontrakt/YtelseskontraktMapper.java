59acc60eaf9e1dfbc3841974ceb85254d4956b29
Hans Petter Simonsen <hans.petter.simonsen@nav.no>
2023-03-21 15:01:31 +0100

    Generer egen BehandleArbeidssoeker for arena soap tjeneste for å unngå javax.xml avhengigheter

--- src/main/java/no/nav/veilarboppfolging/client/ytelseskontrakt/YtelseskontraktMapper.java
+++ src/main/java/no/nav/veilarboppfolging/client/ytelseskontrakt/YtelseskontraktMapper.java
@@ -11,9 +11,12 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Function;
-import java.util.stream.Collectors;
 
 public class YtelseskontraktMapper {
+
+    private YtelseskontraktMapper() {
+
+    }
 
     public static YtelseskontraktResponse tilYtelseskontrakt(HentYtelseskontraktListeResponse response) {
         final List<VedtakDto> vedtakDtoList = mapVedtak(response);
@@ -24,18 +27,18 @@ public class YtelseskontraktMapper {
     private static List<YtelseskontraktDto> mapYtelser(HentYtelseskontraktListeResponse response) {
         return response.getYtelseskontraktListe().stream()
                 .map(YtelseskontraktToYtelseskontrakt)
-                .collect(Collectors.toList());
+                .toList();
     }
 
     private static List<VedtakDto> mapVedtak(HentYtelseskontraktListeResponse response) {
-        final List<Vedtak> VedtakList = response.getYtelseskontraktListe().stream()
+        final List<Vedtak> vedtakList = response.getYtelseskontraktListe().stream()
                 .map(Ytelseskontrakt::getIhtVedtak)
                 .flatMap(Collection::stream)
-                .collect(Collectors.toList());
+                .toList();
 
-        final List<VedtakDto> vedtakDtoList = VedtakList.stream()
+        final List<VedtakDto> vedtakDtoList = vedtakList.stream()
                 .map(VedtakToVedtak)
-                .collect(Collectors.toList());
+                .toList();
 
         setRettighetsgruppePaVedtak(response, vedtakDtoList);
         return vedtakDtoList;
@@ -54,29 +57,29 @@ public class YtelseskontraktMapper {
                 .map(Rettighetsgruppe::getRettighetsGruppe).orElse("");
     }
 
-    private static final Function<Vedtak, VedtakDto> VedtakToVedtak = Vedtak -> {
-        final Optional<XMLGregorianCalendar> fomdato = Optional.ofNullable(Vedtak.getVedtaksperiode().getFom());
-        final Optional<XMLGregorianCalendar> tomdato = Optional.ofNullable(Vedtak.getVedtaksperiode().getTom());
+    private static final Function<Vedtak, VedtakDto> VedtakToVedtak = vedtak -> {
+        final Optional<XMLGregorianCalendar> fomdato = Optional.ofNullable(vedtak.getVedtaksperiode().getFom());
+        final Optional<XMLGregorianCalendar> tomdato = Optional.ofNullable(vedtak.getVedtaksperiode().getTom());
 
         final VedtakDto ytelse = new VedtakDto()
-                .setVedtakstype(Vedtak.getVedtakstype())
-                .setStatus(Vedtak.getStatus())
-                .setAktivitetsfase(Vedtak.getAktivitetsfase());
+                .setVedtakstype(vedtak.getVedtakstype())
+                .setStatus(vedtak.getStatus())
+                .setAktivitetsfase(vedtak.getAktivitetsfase());
 
-        fomdato.ifPresent((fd) -> ytelse.setFradato(new Dato(fd.getYear(), fd.getMonth(), fd.getDay())));
-        tomdato.ifPresent((td) -> ytelse.setTildato(new Dato(td.getYear(), td.getMonth(), td.getDay())));
+        fomdato.ifPresent(fd -> ytelse.setFradato(new Dato(fd.getYear(), fd.getMonth(), fd.getDay())));
+        tomdato.ifPresent(td -> ytelse.setTildato(new Dato(td.getYear(), td.getMonth(), td.getDay())));
 
         return ytelse;
     };
 
-    private static final Function<Ytelseskontrakt, YtelseskontraktDto> YtelseskontraktToYtelseskontrakt = Ytelseskontrakt -> {
-        final Optional<XMLGregorianCalendar> fomGyldighetsperiode = Optional.ofNullable(Ytelseskontrakt.getFomGyldighetsperiode());
-        final Optional<XMLGregorianCalendar> tomGyldighetsperiode = Optional.ofNullable(Ytelseskontrakt.getTomGyldighetsperiode());
+    private static final Function<Ytelseskontrakt, YtelseskontraktDto> YtelseskontraktToYtelseskontrakt = ytelseskontrakt -> {
+        final Optional<XMLGregorianCalendar> fomGyldighetsperiode = Optional.ofNullable(ytelseskontrakt.getFomGyldighetsperiode());
+        final Optional<XMLGregorianCalendar> tomGyldighetsperiode = Optional.ofNullable(ytelseskontrakt.getTomGyldighetsperiode());
 
         final YtelseskontraktDto ytelseskontraktDto = new YtelseskontraktDto()
-                .withYtelsestype(Ytelseskontrakt.getYtelsestype())
-                .withStatus(Ytelseskontrakt.getStatus())
-                .withDatoMottatt(Ytelseskontrakt.getDatoKravMottatt());
+                .withYtelsestype(ytelseskontrakt.getYtelsestype())
+                .withStatus(ytelseskontrakt.getStatus())
+                .withDatoMottatt(ytelseskontrakt.getDatoKravMottatt());
 
         fomGyldighetsperiode.ifPresent(ytelseskontraktDto::setDatoFra);
         tomGyldighetsperiode.ifPresent(ytelseskontraktDto::setDatoTil);
