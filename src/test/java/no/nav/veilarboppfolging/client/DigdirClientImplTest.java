47abf22e4eab5119c40325f237d8c6c6b8f6eeab
JulieHillRoa <julie.hill.roa@nav.no>
2023-05-03 14:04:07 +0200

    Fikse digdir-tester

--- src/test/java/no/nav/veilarboppfolging/client/DigdirClientImplTest.java
+++ src/test/java/no/nav/veilarboppfolging/client/DigdirClientImplTest.java
@@ -1,10 +1,10 @@
 package no.nav.veilarboppfolging.client;
 
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import no.nav.common.sts.SystemUserTokenProvider;
 import no.nav.veilarboppfolging.client.digdir_krr.DigdirClient;
 import no.nav.veilarboppfolging.client.digdir_krr.DigdirClientImpl;
 import no.nav.veilarboppfolging.client.digdir_krr.DigdirKontaktinfo;
-import no.nav.veilarboppfolging.test.TestData;
 import no.nav.veilarboppfolging.test.TestUtils;
 import org.junit.Rule;
 import org.junit.Test;
@@ -12,22 +12,23 @@ import org.junit.Test;
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static no.nav.veilarboppfolging.test.TestData.TEST_FNR;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
 
 public class DigdirClientImplTest {
 
     @Rule
     public WireMockRule wireMockRule = new WireMockRule(0);
 
-    @Test
+	private SystemUserTokenProvider systemUserTokenProvider = mock(SystemUserTokenProvider.class);
+	@Test
     public void hentKontaktInfo__skal_hente_kontaktinfo() {
         String kodeverkJson = TestUtils.readTestResourceFile("client/digdir/kontaktinfo.json");
         String apiUrl = "http://localhost:" + wireMockRule.port();
         DigdirClient digdirClient = new DigdirClientImpl(apiUrl, () -> "TOKEN");
 
         givenThat(get(anyUrl())
-                .withHeader("Nav-Personident", equalTo(TEST_FNR.get()))
-                .withHeader("Authorization", equalTo("Bearer TOKEN"))
-                .withHeader("Nav-call-id", equalTo(TestData.uuid.toString()))
+				.withHeader("Nav-personident", equalTo(TEST_FNR.get()))
+				.withHeader("Authorization", equalTo("Bearer TOKEN"))
                 .willReturn(aResponse()
                         .withStatus(200)
                         .withBody(kodeverkJson))
@@ -49,7 +50,7 @@ public class DigdirClientImplTest {
 
         givenThat(get(anyUrl())
                 .withHeader("Nav-Personident", equalTo(TEST_FNR.get()))
-                .withHeader("Nav-call-id", equalTo("Bearer TOKEN"))
+				.withHeader("Authorization", equalTo("Bearer TOKEN"))
                 .willReturn(aResponse()
                         .withStatus(200)
                         .withBody(kodeverkJson))
