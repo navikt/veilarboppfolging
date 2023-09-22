53ef481559c131bc8c215eec8aed2ca0cc8ce73f
Klara M Helgemo <klara.margrethe.helgemo@nav.no>
2023-09-22 15:24:37 +0200

    Ny post-mapping "v3" med fnr i data

--- src/main/java/no/nav/veilarboppfolging/controller/v2/OppfolgingV2Controller.java
+++ src/main/java/no/nav/veilarboppfolging/controller/v2/OppfolgingV2Controller.java
@@ -134,9 +134,9 @@ public class OppfolgingV2Controller {
         return periode.toBuilder().kvpPerioder(kvpPeriodeEntities).build();
     }
     @PostMapping ("/v3")
-    public OppfolgingStatus hentOppfolgingData(@RequestBody  Fnr fnr) {
-        secureLog.info("v3 postmapping innsendt ident: {}", fnr.get());
-        Fnr fodselsnummer = authService.hentIdentForEksternEllerIntern(fnr);
+    public OppfolgingStatus hentOppfolgingData(@RequestBody  String fnr) {
+        secureLog.info("v3 postmapping innsendt ident: {}", fnr);
+        Fnr fodselsnummer = authService.hentIdentForEksternEllerIntern(Fnr.of(fnr));
         return tilDto(oppfolgingService.hentOppfolgingsStatus(fodselsnummer), authService.erInternBruker());
     }
 }
