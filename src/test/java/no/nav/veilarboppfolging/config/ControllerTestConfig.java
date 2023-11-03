20761eaa1f17308c930a3083a258f56a6b0d3491
Klara M Helgemo <klara.margrethe.helgemo@nav.no>
2023-11-03 09:35:22 +0100

    Tester

--- src/test/java/no/nav/veilarboppfolging/config/ControllerTestConfig.java
+++ src/test/java/no/nav/veilarboppfolging/config/ControllerTestConfig.java
@@ -1,12 +1,14 @@
 package no.nav.veilarboppfolging.config;
 
 import no.nav.veilarboppfolging.controller.*;
+import no.nav.veilarboppfolging.controller.v2.ArenaOppfolgingV2Controller;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
 @Import({
         ArenaOppfolgingController.class,
+        ArenaOppfolgingV2Controller.class,
         InternalController.class,
         KvpController.class,
         MaalController.class,
