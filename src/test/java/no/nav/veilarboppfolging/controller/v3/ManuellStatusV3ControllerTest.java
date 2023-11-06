c72fee36da89f32fefd6fff924dc4de09f58eeb0
Oda Dahlen <oda.dahlen@nav.no>
2023-11-06 13:36:07 +0100

    lagt til alle tester for ManuellStatusV3Controller

--- src/test/java/no/nav/veilarboppfolging/controller/v3/ManuellStatusV3ControllerTest.java
+++ src/test/java/no/nav/veilarboppfolging/controller/v3/ManuellStatusV3ControllerTest.java
@@ -2,6 +2,7 @@ package no.nav.veilarboppfolging.controller.v3;
 
 import no.nav.common.json.JsonUtils;
 import no.nav.common.types.identer.Fnr;
+import no.nav.veilarboppfolging.client.digdir_krr.DigdirKontaktinfo;
 import no.nav.veilarboppfolging.controller.v3.request.ManuellStatusRequest;
 import no.nav.veilarboppfolging.service.AuthService;
 import no.nav.veilarboppfolging.service.ManuellStatusService;
@@ -43,6 +44,50 @@ public class ManuellStatusV3ControllerTest {
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(JsonUtils.toJson(manuellStatusRequest))).andExpect(content().json(expJson));
 
+    }
+
+    @Test
+    public void manuell_hent_status_skal_returnere_riktig() throws Exception {
+        when(manuellStatusService.erManuell((Fnr) any())).thenReturn(true);
+        String expJson = "{\"erUnderManuellOppfolging\":true}, \"krrStatus\":{\"kanVarsles\":true, \"erReservert\":false}";
+        when(manuellStatusService.hentDigdirKontaktinfo(manuellStatusRequest.fnr())).thenReturn(new DigdirKontaktinfo().setReservert(false).setKanVarsles(true));
+
+        mockMvc.perform(post("/api/v3/manuell/hent-status")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtils.toJson(manuellStatusRequest))).andExpect(content().json(expJson));
+    }
+
+
+    @Test
+    public void sett_digital_ekstern() throws Exception {
+        when(authService.erEksternBruker()).thenReturn(true);
+        mockMvc.perform(post("/api/v3/manuell/sett-digital")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtils.toJson(manuellStatusRequest))
+        );
+
+        verify(manuellStatusService, times(1)).settDigitalBruker(any());
+    }
+
+    @Test
+    public void sett_digital_ikke_ekstern() throws Exception {
+        when(authService.erEksternBruker()).thenReturn(false);
+        mockMvc.perform(post("/api/v3/manuell/sett-digital")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtils.toJson(manuellStatusRequest))
+        );
+
+        verify(manuellStatusService, times(0)).settDigitalBruker(any());
+    }
+
+    @Test
+    public void sett_manuell() throws Exception {
+        when(authService.erEksternBruker()).thenReturn(true);
+        ResultActions result = mockMvc.perform(post("/api/v3/manuell/sett-manuell")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtils.toJson(manuellStatusRequest))
+        );
+        assert(result.andReturn().getResponse().getStatus() == 200);
     }
 
     @Test
