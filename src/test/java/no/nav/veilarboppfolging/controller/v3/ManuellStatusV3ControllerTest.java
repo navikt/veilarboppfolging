756b62f4975a02bf6991b45e31055a653586b4a1
Oda Dahlen <oda.dahlen@nav.no>
2023-11-06 12:54:44 +0100

    lagt til test for manuellstatuscontroller

--- src/test/java/no/nav/veilarboppfolging/controller/v3/ManuellStatusV3ControllerTest.java
+++ src/test/java/no/nav/veilarboppfolging/controller/v3/ManuellStatusV3ControllerTest.java
@@ -1,6 +1,7 @@
 package no.nav.veilarboppfolging.controller.v3;
 
 import no.nav.common.json.JsonUtils;
+import no.nav.common.types.identer.Fnr;
 import no.nav.veilarboppfolging.controller.v3.request.ManuellStatusRequest;
 import no.nav.veilarboppfolging.service.AuthService;
 import no.nav.veilarboppfolging.service.ManuellStatusService;
@@ -10,11 +11,14 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
 
 import static no.nav.veilarboppfolging.test.TestData.TEST_FNR;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 @WebMvcTest(controllers = ManuellStatusV3Controller.class)
 public class ManuellStatusV3ControllerTest {
@@ -31,6 +35,15 @@ public class ManuellStatusV3ControllerTest {
 
     ManuellStatusRequest manuellStatusRequest = new ManuellStatusRequest(TEST_FNR);
 
+    @Test
+    public void hent_manuell_skal_returnere_true() throws Exception {
+        when(manuellStatusService.erManuell((Fnr) any())).thenReturn(true);
+        String expJson = "{\"erUnderManuellOppfolging\":true}";
+        mockMvc.perform(post("/api/v3/hent-manuell")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtils.toJson(manuellStatusRequest))).andExpect(content().json(expJson));
+
+    }
 
     @Test
     public void synkroniserManuellStatusMedDigdir__should_only_be_used_by_internal_users() throws Exception {
--- src/test/java/no/nav/veilarboppfolging/controller/v3/VeilederV3ControllerTest.java
+++ src/test/java/no/nav/veilarboppfolging/controller/v3/VeilederV3ControllerTest.java
@@ -13,8 +13,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.ResultActions;
 
 import java.util.Optional;
 
