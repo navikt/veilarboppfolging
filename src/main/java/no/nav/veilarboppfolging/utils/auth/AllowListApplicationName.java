5d8e6fe0b83e86b482bd4fba911268024da867cf
Sondre Larsen Ovrid <sondre.larsen.ovrid@nav.no>
2023-11-10 11:02:49 +0100

    Legg til privat noop-konskrutør for klasse som ikke skal kunne instansieres

--- src/main/java/no/nav/veilarboppfolging/utils/auth/AllowListApplicationName.java
+++ src/main/java/no/nav/veilarboppfolging/utils/auth/AllowListApplicationName.java
@@ -1,6 +1,9 @@
 package no.nav.veilarboppfolging.utils.auth;
 
 public class AllowListApplicationName {
+
+    private AllowListApplicationName() {}
+
     public static String VEILARBVEDTAKSSTOTTE = "veilarbvedtaksstotte";
     public static String VEILARBDIALOG = "veilarbdialog";
     public static String VEILARBAKTIVITET = "veilarbaktivitet";
