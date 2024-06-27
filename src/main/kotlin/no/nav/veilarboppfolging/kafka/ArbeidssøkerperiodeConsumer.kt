c7e2be8bd4c0b0b9d9e5b548f214cf410b4a7f56
Mads Lee Giil <mads.lee.giil@nav.no>
2024-06-27 08:16:55 +0200

    Logg for å verifisere at konsumering starter i dev

--- src/main/kotlin/no/nav/veilarboppfolging/kafka/ArbeidssøkerperiodeConsumer.kt
+++ src/main/kotlin/no/nav/veilarboppfolging/kafka/ArbeidssøkerperiodeConsumer.kt
@@ -2,12 +2,15 @@ package no.nav.veilarboppfolging.kafka
 
 import no.nav.paw.arbeidssokerregisteret.api.v1.Periode
 import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
 class ArbeidssøkerperiodeConsumer {
 
+    private val logger = LoggerFactory.getLogger(this::class.java)
+
     fun consumeArbeidssøkerperiode(kafkaMelding: ConsumerRecord<String, Periode>) {
-        TODO("Not implemented - expect test to fail")
+        logger.info("Hopper over melding fra nytt arbeidssøkerregister")
     }
 }
