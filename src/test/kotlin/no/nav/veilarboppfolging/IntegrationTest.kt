f97d6d0cfc5fd98f60ab7f5064fc46271c91cf2e
johannetronstad <johanne.tronstad@nav.no>
2024-09-12 12:44:08 +0200

    Enable kafka i test

--- src/test/kotlin/no/nav/veilarboppfolging/IntegrationTest.kt
+++ src/test/kotlin/no/nav/veilarboppfolging/IntegrationTest.kt
@@ -26,6 +26,7 @@ import org.mockito.Mockito
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.context.annotation.Profile
 import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
 import org.springframework.kafka.test.context.EmbeddedKafka
