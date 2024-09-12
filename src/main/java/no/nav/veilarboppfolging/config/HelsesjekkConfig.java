c60b1dd466f80ea41410d9e79ee5759f772acc50
sigurdgroneng <sigurd.groneng@nav.no>
2024-09-12 15:59:14 +0200

    Fix trailing comma

--- src/main/java/no/nav/veilarboppfolging/config/HelsesjekkConfig.java
+++ src/main/java/no/nav/veilarboppfolging/config/HelsesjekkConfig.java
@@ -22,7 +22,7 @@ public class HelsesjekkConfig {
     public SelfTestChecks selfTestChecks(JdbcTemplate jdbcTemplate,
                                          AktorOppslagClient aktorOppslagClient) {
         List<SelfTestCheck> selfTestChecks = Arrays.asList(
-                new SelfTestCheck("Enkel spørring mot Databasen til veilarboppfolging.", true, checkDbHealth(jdbcTemplate)),
+                new SelfTestCheck("Enkel spørring mot Databasen til veilarboppfolging.", true, checkDbHealth(jdbcTemplate))
                 // TODO selftest poao-tilgang
 //                new SelfTestCheck("Ping av aktor oppslag client (konvertere mellom aktorId og Fnr).", true, aktorOppslagClient)
         );
