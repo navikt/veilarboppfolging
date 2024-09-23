f0540c9540bc2e796d8103b1e7b4a8da4f005222
sigurdgroneng <sigurd.groneng@nav.no>
2024-09-23 16:38:34 +0200

    Legg til pdl-url og scope

--- src/main/java/no/nav/veilarboppfolging/config/ClientConfig.java
+++ src/main/java/no/nav/veilarboppfolging/config/ClientConfig.java
@@ -13,21 +13,15 @@ import no.nav.common.token_client.client.AzureAdMachineToMachineTokenClient;
 import no.nav.common.token_client.client.AzureAdOnBehalfOfTokenClient;
 import no.nav.common.token_client.client.MachineToMachineTokenClient;
 import no.nav.common.token_client.client.TokenXOnBehalfOfTokenClient;
-import no.nav.common.utils.EnvironmentUtils;
 import no.nav.veilarboppfolging.client.amttiltak.AmtTiltakClient;
 import no.nav.veilarboppfolging.client.digdir_krr.DigdirClient;
 import no.nav.veilarboppfolging.client.digdir_krr.DigdirClientImpl;
 import no.nav.veilarboppfolging.client.veilarbarena.VeilarbarenaClient;
 import no.nav.veilarboppfolging.client.veilarbarena.VeilarbarenaClientImpl;
 import no.nav.veilarboppfolging.service.AuthService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import static no.nav.common.utils.UrlUtils.createNaisAdeoIngressUrl;
-import static no.nav.common.utils.UrlUtils.createNaisPreprodIngressUrl;
-import static no.nav.common.utils.UrlUtils.createServiceUrl;
 
 @Configuration
 public class ClientConfig {
@@ -36,17 +30,16 @@ public class ClientConfig {
     private String veilarbarenaUrl;
     @Value("${app.env.veilarbarenaAzureScope}")
     private String veilarbarenaAzureScope;
+    @Value("${app.env.pdlUrl}")
+    private String pdlUrl;
+    @Value("${app.env.pdlScope}")
+    private String pdlScope;
 
     @Bean
     public AktorOppslagClient aktorOppslagClient(MachineToMachineTokenClient tokenClient) {
-        String tokenScop = String.format("api://%s-fss.pdl.pdl-api/.default",
-                isProduction() ? "prod" : "dev"
-        );
-
         PdlAktorOppslagClient pdlClient = new PdlAktorOppslagClient(
-                createServiceUrl("pdl-api", "pdl", false),
-                () -> tokenClient.createMachineToMachineToken(tokenScop));
-
+                pdlUrl,
+                () -> tokenClient.createMachineToMachineToken(pdlScope));
         return new CachedAktorOppslagClient(pdlClient);
     }
 
@@ -90,15 +83,4 @@ public class ClientConfig {
                 .withNaisDefaults()
                 .buildOnBehalfOfTokenClient();
     }
-
-    private static String naisPreprodOrNaisAdeoIngress(String appName, boolean withAppContextPath) {
-        return EnvironmentUtils.isProduction().orElse(false)
-                ? createNaisAdeoIngressUrl(appName, withAppContextPath)
-                : createNaisPreprodIngressUrl(appName, "q1", withAppContextPath);
-    }
-
-    private static boolean isProduction() {
-        return EnvironmentUtils.isProduction().orElseThrow();
-    }
-
 }
