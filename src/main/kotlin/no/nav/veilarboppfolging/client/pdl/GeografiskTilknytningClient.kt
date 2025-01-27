66e8b9d68cfaa90acfb84064c125b046bdb5b34b
sigurdgroneng <sigurd.groneng@nav.no>
2025-01-27 15:20:23 +0100

    Client som henter GT fra pdl

---
+++ src/main/kotlin/no/nav/veilarboppfolging/client/pdl/GeografiskTilknytningClient.kt
@@ -0,0 +1,38 @@
+package no.nav.veilarboppfolging.client.pdl
+
+import no.nav.common.client.pdl.PdlClient
+import no.nav.common.client.utils.graphql.GraphqlRequestBuilder
+import no.nav.common.client.utils.graphql.GraphqlResponse
+import no.nav.common.client.utils.graphql.GraphqlUtils
+import no.nav.common.types.identer.Fnr
+import org.springframework.stereotype.Service
+
+@Service
+class GeografsiskTilknytningClient(val pdlClient: PdlClient) {
+
+    fun hentGeografiskTilknytning(fnr: Fnr): GeografiskTilknytning {
+        val graphqlRequest = GraphqlRequestBuilder<QueryVariables>("graphql/pdl/hentGeografiskTilknytning.graphql")
+            .buildRequest(QueryVariables(ident = fnr.get()))
+        val gtResult = pdlClient.request(graphqlRequest, GeografiskTilknytningResponse::class.java)
+            .also { GraphqlUtils.logWarningIfError(it) }
+        if(gtResult.errors?.isNotEmpty() == true) { throw RuntimeException("Feil ved kall til pdl") }
+        return gtResult.data
+    }
+}
+
+data class QueryVariables(
+    val ident: String,
+)
+
+enum class GTType {
+    BYDEL, KOMMUNE, UDEFINERT, UTLAND
+}
+
+data class GeografiskTilknytning(
+    val gtType: GTType,
+    val gtKommune: String?,
+    val gtBydel: String?,
+    val gtLand: String?,
+)
+
+class GeografiskTilknytningResponse: GraphqlResponse<GeografiskTilknytning>()
