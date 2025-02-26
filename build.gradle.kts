d938a5facc24eb154f4b3ab59c63879699aa9fed
sigurdgroneng <sigurd.groneng@nav.no>
2025-02-26 12:24:02 +0100

    Fix build dir

--- build.gradle.kts
+++ build.gradle.kts
@@ -125,7 +125,7 @@ sonarqube {
         property("sonar.projectKey", "navikt_veilarboppfolging")
         property("sonar.organization", "navikt")
         property("sonar.host.url", "https://sonarcloud.io")
-        property("sonar.java.binaries", "${project.layout.buildDirectory}/classes/java/main")
+        property("sonar.java.binaries", "${project.layout.buildDirectory.get().asFile}/classes/java/main")
     }
 }
 
