0e36c8ccee1a974240ad79a6599cf14774ede22d
erikbosnes <erik.bosnes@nav.no>
2025-03-14 14:48:18 +0100

    Aktiver microfrontender

--- src/main/kotlin/no/nav/veilarboppfolging/service/KafkaMicrofrontendService.kt
+++ src/main/kotlin/no/nav/veilarboppfolging/service/KafkaMicrofrontendService.kt
@@ -18,22 +18,18 @@ class KafkaMicrofrontendService (
 
     val logger = LoggerFactory.getLogger(this::class.java)
 
-    @Scheduled(cron = "0 45 14 * * *")
+    @Scheduled(cron = "0 55 14 * * *")
     fun aktiverMicrofrontendForBrukereUnderOppfolging() {
 
         var microfrontendEntities = oppfolgingsPeriodeRepository.hentAlleSomSkalAktiveres()
 
-        logger.info("Fant ${microfrontendEntities.count()} brukere som skal aktiveres.")
-
-        var utvalgteEntities = microfrontendEntities.takeLast(10)
-
-        for(microfrontendEntity in utvalgteEntities) {
+        for(microfrontendEntity in microfrontendEntities) {
             try {
                 kafkaProducerService.publiserVisAoMinSideMicrofrontend(AktorId.of(microfrontendEntity.aktorId))
-                logger.info("Har sendt aktiver-melding for ${microfrontendEntity.aktorId}")
+//                logger.info("Har sendt aktiver-melding for ${microfrontendEntity.aktorId}")
 
                 oppfolgingsPeriodeRepository.aktiverMicrofrontend(AktorId.of(microfrontendEntity.aktorId))
-                logger.info("Har lagret aktiver-status for ${microfrontendEntity.aktorId}")
+//                logger.info("Har lagret aktiver-status for ${microfrontendEntity.aktorId}")
             } catch (e: Exception) {
                 logger.warn("Dette gikk dårlig gitt: ${microfrontendEntity.aktorId}. Exception: ${e.message}")
             }
