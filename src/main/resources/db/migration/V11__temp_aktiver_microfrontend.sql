00331b7e3aaf819e377bbf40cf8ee24b3fea1c16
erikbosnes <erik.bosnes@nav.no>
2025-03-17 13:49:34 +0100

    Aktiver microfrontender

--- src/main/resources/db/migration/V11__temp_aktiver_microfrontend.sql
+++ src/main/resources/db/migration/V11__temp_aktiver_microfrontend.sql
@@ -1,12 +1,12 @@
 create table temp_aktiver_microfrontend (
-    aktor_id varchar(20),
+    aktor_id varchar(20) primary key,
     status varchar(20),
     startdato_oppfolging timestamp(6),
     melding text
 );
 
 create table temp_deaktiver_microfrontend (
-    aktor_id varchar(20),
+    aktor_id varchar(20) primary key,
     status varchar(20),
     sluttdato_oppfolging timestamp(6),
     melding text
