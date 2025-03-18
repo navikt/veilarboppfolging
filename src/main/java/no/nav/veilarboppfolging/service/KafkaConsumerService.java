a0ea76c114051951b3fa3141c4e42aef918b02d1
Hans Petter Simonsen <hans.petter.simonsen@nav.no>
2025-03-18 12:34:54 +0100

    Securelog feilende ident for IngenGjeldendeIdentException

--- src/main/java/no/nav/veilarboppfolging/service/KafkaConsumerService.java
+++ src/main/java/no/nav/veilarboppfolging/service/KafkaConsumerService.java
@@ -11,6 +11,7 @@ import no.nav.common.client.aktorregister.IngenGjeldendeIdentException;
 import no.nav.common.types.identer.Fnr;
 import no.nav.pto_schema.kafka.json.topic.onprem.EndringPaaOppfoelgingsBrukerV2;
 import no.nav.veilarboppfolging.service.utmelding.KanskjeIservBruker;
+import no.nav.veilarboppfolging.utils.SecureLog;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
@@ -83,6 +84,7 @@ public class KafkaConsumerService {
             sisteEndringPaaOppfolgingBrukerService.lagreSisteEndring(brukerFnr, endringPaBruker.getSistEndretDato());
         } catch (IngenGjeldendeIdentException e) {
             log.warn("Fant ikke gjeldende ident ved behandling av endringPaOppfolgingBruker melding");
+            SecureLog.secureLog.warn("Fant ikke gjeldende ident for fnr: {} ved behandling av endringPaOppfolgingBruker melding", brukerFnr.get());
         }
     }
 
