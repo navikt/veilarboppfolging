28bc55e737091990b4a8774326955a2b5311e5b0
johannetronstad <johanne.tronstad@nav.no>
2025-04-30 14:48:35 +0200

    Refactor ReaktiveringServiceTest to use constructor injection and add error handling tests

--- src/test/kotlin/no/nav/veilarboppfolging/service/ReaktiveringServiceTest.kt
+++ src/test/kotlin/no/nav/veilarboppfolging/service/ReaktiveringServiceTest.kt
@@ -2,11 +2,8 @@ package no.nav.veilarboppfolging.service
 
 import no.nav.common.types.identer.AktorId
 import no.nav.common.types.identer.Fnr
-import no.nav.veilarboppfolging.client.veilarbarena.ARENA_REGISTRERING_RESULTAT
-import no.nav.veilarboppfolging.client.veilarbarena.ReaktiveringResult
-import no.nav.veilarboppfolging.client.veilarbarena.ReaktiveringSuccess
-import no.nav.veilarboppfolging.client.veilarbarena.RegistrerIArenaSuccess
-import no.nav.veilarboppfolging.client.veilarbarena.RegistrerIkkeArbeidssokerDto
+import no.nav.veilarboppfolging.LocalDatabaseSingleton
+import no.nav.veilarboppfolging.client.veilarbarena.*
 import no.nav.veilarboppfolging.domain.StartetAvType
 import no.nav.veilarboppfolging.oppfolgingsbruker.arena.ArenaOppfolgingService
 import no.nav.veilarboppfolging.repository.OppfolgingsPeriodeRepository
@@ -14,47 +11,42 @@ import no.nav.veilarboppfolging.repository.OppfolgingsStatusRepository
 import no.nav.veilarboppfolging.repository.ReaktiveringRepository
 import no.nav.veilarboppfolging.repository.entity.OppfolgingEntity
 import no.nav.veilarboppfolging.repository.entity.OppfolgingsperiodeEntity
-import org.checkerframework.checker.initialization.qual.Initialized
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
+import org.junit.jupiter.api.assertThrows
 import org.junit.runner.RunWith
 import org.mockito.ArgumentMatchers.any
-import org.mockito.InjectMocks
-import org.mockito.Mock
 import org.mockito.Mockito
 import org.mockito.Mockito.doAnswer
 import org.mockito.Mockito.mock
 import org.mockito.junit.MockitoJUnitRunner
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
 import org.springframework.transaction.TransactionStatus
 import org.springframework.transaction.support.TransactionCallback
 import org.springframework.transaction.support.TransactionTemplate
+import org.springframework.web.server.ResponseStatusException
 import java.time.ZonedDateTime
 import java.util.*
-import java.util.function.Consumer
 
 @RunWith(MockitoJUnitRunner::class)
 class ReaktiveringServiceTest {
-    @Mock
-    private lateinit var authService: AuthService
-
-    @Mock
-    private lateinit var reaktiveringRepository: ReaktiveringRepository
-
-    @Mock
-    private lateinit var oppfolgingsStatusRepository: OppfolgingsStatusRepository
-
-    @Mock
-    private lateinit var arenaOppfolgingService: ArenaOppfolgingService
-
-    @Mock
-    private lateinit var oppfolgingsPeriodeRepository: OppfolgingsPeriodeRepository
-
-    @Mock
-    private lateinit var transactor: TransactionTemplate
-
-    @InjectMocks
-    private lateinit var reaktiveringService: ReaktiveringService
+    private var authService: AuthService = mock(AuthService::class.java)
+    private val reaktiveringRepository =
+        ReaktiveringRepository(NamedParameterJdbcTemplate(LocalDatabaseSingleton.jdbcTemplate))
+    private var oppfolgingsStatusRepository: OppfolgingsStatusRepository = mock(OppfolgingsStatusRepository::class.java)
+    private var arenaOppfolgingService: ArenaOppfolgingService = mock(ArenaOppfolgingService::class.java)
+    private var oppfolgingsPeriodeRepository: OppfolgingsPeriodeRepository =
+        mock(OppfolgingsPeriodeRepository::class.java)
+    private var transactor: TransactionTemplate = mock(TransactionTemplate::class.java)
+    private val reaktiveringService = ReaktiveringService(
+        authService,
+        oppfolgingsStatusRepository,
+        arenaOppfolgingService,
+        reaktiveringRepository,
+        oppfolgingsPeriodeRepository,
+        transactor
+    )
 
     @Before
     fun setup() {
@@ -71,7 +63,6 @@ class ReaktiveringServiceTest {
 
     @Test
     fun `skal reaktivere bruker hvis den er inaktivert i Arena og under arbeidsrettet oppfølging`() {
-
         Mockito.`when`<Optional<OppfolgingEntity>?>(oppfolgingsStatusRepository.hentOppfolging(AKTOR_ID))
             .thenReturn(
                 Optional.of<OppfolgingEntity>(OppfolgingEntity().setUnderOppfolging(true))
@@ -93,9 +84,49 @@ class ReaktiveringServiceTest {
         val resultat = reaktiveringService.reaktiverBrukerIArena(FNR)
 
         Assert.assertTrue(resultat is ReaktiveringSuccess)
+        Assert.assertTrue((resultat as ReaktiveringSuccess).reaktiveringResponse.kode == REAKTIVERING_RESULTAT.OK_REGISTRERT_I_ARENA)
+        Assert.assertTrue((resultat).reaktiveringResponse.ok == true)
 
+        val reaktiveringHistorikk = reaktiveringRepository.hentReaktiveringer(AKTOR_ID)
+        Assert.assertTrue(reaktiveringHistorikk.size == 1)
     }
 
+    @Test
+    fun `skal gi feil hvis bruker ikke er under oppfølging`() {
+        Mockito.`when`<Optional<OppfolgingEntity>?>(oppfolgingsStatusRepository.hentOppfolging(AKTOR_ID))
+            .thenReturn(
+                Optional.of<OppfolgingEntity>(OppfolgingEntity().setUnderOppfolging(false))
+            )
+
+        val resultat = reaktiveringService.reaktiverBrukerIArena(FNR)
+
+        Assert.assertTrue(resultat is ReaktiveringSuccess)
+        Assert.assertTrue((resultat as ReaktiveringSuccess).reaktiveringResponse.kode == REAKTIVERING_RESULTAT.KAN_IKKE_REAKTIVERES)
+        Assert.assertTrue((resultat).reaktiveringResponse.ok == false)
+
+        val reaktiveringHistorikk = reaktiveringRepository.hentReaktiveringer(AKTOR_ID)
+        Assert.assertTrue(reaktiveringHistorikk.isEmpty())
+    }
+
+    @Test
+    fun `skal gi error hvis arena kall feiler`() {
+        Mockito.`when`<Optional<OppfolgingEntity>?>(oppfolgingsStatusRepository.hentOppfolging(AKTOR_ID))
+            .thenReturn(
+                Optional.of<OppfolgingEntity>(OppfolgingEntity().setUnderOppfolging(true))
+            )
+
+        Mockito.`when`(arenaOppfolgingService.registrerIkkeArbeidssoker(FNR)).thenReturn(
+            RegistrerIArenaError(
+                "Feil ved registrering av bruker i Arena",
+                RuntimeException("Feil ved registrering av bruker i Arena")
+            )
+        )
+
+        assertThrows<ResponseStatusException> { reaktiveringService.reaktiverBrukerIArena(FNR) }
+
+        val reaktiveringHistorikk = reaktiveringRepository.hentReaktiveringer(AKTOR_ID)
+        Assert.assertTrue(reaktiveringHistorikk.isEmpty())
+    }
 
     private fun mockStartetOppfolgingsperiode(aktorId: AktorId): OppfolgingsperiodeEntity {
         return OppfolgingsperiodeEntity(
@@ -116,9 +147,6 @@ class ReaktiveringServiceTest {
         private val FNR: Fnr = Fnr.of("fnr")
         private val AKTOR_ID: AktorId = AktorId.of("aktorId")
         private val VEILEDER_IDENT: String = "Z123456"
-
         private val OPPFOLGING_START: ZonedDateTime = ZonedDateTime.now().minusDays(10)
-        private val OPPFOLGING_END: ZonedDateTime = OPPFOLGING_START.plusDays(5)
-        private val OPPFOLGING_REAKTIVERT: ZonedDateTime = OPPFOLGING_START.plusDays(3)
     }
 }
