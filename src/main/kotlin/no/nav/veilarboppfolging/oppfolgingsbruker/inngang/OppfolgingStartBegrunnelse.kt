fb96674c1be64a3110eef74b169684a7eaa9c516
Hans Petter Simonsen <hans.petter.simonsen@nav.no>
2025-05-16 13:05:55 +0200

    Dokumentasjon av OppfolgingStartBegrunnelse og fjerne ubrukt enum

--- src/main/kotlin/no/nav/veilarboppfolging/oppfolgingsbruker/inngang/OppfolgingStartBegrunnelse.kt
+++ src/main/kotlin/no/nav/veilarboppfolging/oppfolgingsbruker/inngang/OppfolgingStartBegrunnelse.kt
@@ -4,10 +4,22 @@ import no.nav.veilarboppfolging.kafka.dto.StartetBegrunnelseDTO
 
 enum class OppfolgingStartBegrunnelse {
     ARBEIDSSOKER_REGISTRERING,
+    /* Oppfølging startet på grunn av melding fra arbeidssøkerregisteret.
+       startet_av_type vil indikere om det var bruker, veileder eller system som utførte registreringen */
     ARENA_SYNC_ARBS,
+    /* Oppfølging startet av at bruker fikk formidlingsgruppe ARBS i Arena
+       (dette skal normalt ikke skje lenger,
+       siden vi reagerer direkte på arbeidssøkerregistreringen)
+    */
     ARENA_SYNC_IARBS,
-    MANUELL_REGISTRERING_VEILEDER,
-    REAKTIVERT_OPPFØLGING;
+    /* For brukere med fomidlingsgruppe IARBS i Arena,
+       vil oppfølging startes enten når de får en innsatsgruppe
+       (i praksis når 14a vedtak ble gjort i Arena - dette gjøres ikke lenger etter ny vedtaksløsning ble lansert)
+       eller at de får servicegruppe VURDU (sykemeldt uten arbeidsgiver)
+       eller servicegruppe OPPFI (helserelatert arbeidsrettet oppfølging i Nav)
+       */
+    MANUELL_REGISTRERING_VEILEDER;
+    /* Oppfølging startet av veileder via registreringsløsningen inngar */
 
     fun toStartetBegrunnelseDTO(): StartetBegrunnelseDTO {
         if (this == ARBEIDSSOKER_REGISTRERING || this == ARENA_SYNC_ARBS) {
