df2d558c5193145e991ad7cae83b3aa93aa28059
Kristina Skatun <kristinaskatun@gmail.com>
2025-09-29 13:52:35 +0200

    Endre variabelnavn til å være lik den i kafka

--- src/main/java/no/nav/veilarboppfolging/controller/response/Veilarbportefoljeinfo.java
+++ src/main/java/no/nav/veilarboppfolging/controller/response/Veilarbportefoljeinfo.java
@@ -17,5 +17,5 @@ public class Veilarbportefoljeinfo {
     private boolean nyForVeileder;
     private boolean erManuell;
     private ZonedDateTime startDato;
-    private ZonedDateTime sistTilordnetDato;
+    private ZonedDateTime tilordnetTidspunkt;
 }
