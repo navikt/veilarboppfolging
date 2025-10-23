5d8660ebb79d2505262dea9aef06c9c432c2a06a
Giil <mads.lee.giil@nav.no>
2025-10-23 13:18:14 +0200

    WIP: Dataklasser for melding ut

--- src/main/kotlin/no/nav/veilarboppfolging/kafka/ArbeidsoppfolgingskontortilordningConsumerService.kt
+++ src/main/kotlin/no/nav/veilarboppfolging/kafka/ArbeidsoppfolgingskontortilordningConsumerService.kt
@@ -1,8 +1,14 @@
 package no.nav.veilarboppfolging.kafka
 
+import com.fasterxml.jackson.annotation.JsonSubTypes
+import com.fasterxml.jackson.annotation.JsonTypeInfo
+import no.nav.veilarboppfolging.oppfolgingsperioderHendelser.hendelser.HendelseType
+import no.nav.veilarboppfolging.oppfolgingsperioderHendelser.hendelser.OppfolgingStartetHendelseDto
+import no.nav.veilarboppfolging.oppfolgingsperioderHendelser.hendelser.OppfolgingsAvsluttetHendelseDto
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
+import java.time.ZonedDateTime
 import java.util.*
 
 @Service
@@ -11,7 +17,8 @@ class ArbeidsoppfolgingskontortilordningConsumerService {
     private val logger = LoggerFactory.getLogger(this::class.java)
 
     fun consumeKontortilordning(kafkaMelding: ConsumerRecord<String, OppfolgingskontorMelding?>) {
-        logger.info("Kom inn til funksjon for konsumering av melding")
+        // Hvis tombStone -> ignorer fordi vi har sendt avslutttetMelding når perioden ble avslutta
+        // Hvis ikke tombStone -> publiser melding med oppføgingsperiodeStart
     }
 }
 
@@ -21,4 +28,30 @@ data class OppfolgingskontorMelding(
     val oppfolgingsperiodeId: UUID,
     val aktorId: String,
     val ident: String
+)
+
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.EXISTING_PROPERTY, property = "hendelseType")
+@JsonSubTypes(
+    JsonSubTypes.Type(value = OppfolgingStartetHendelseDto::class, name = "OPPFOLGING_STARTET"),
+    JsonSubTypes.Type(value = OppfolgingsAvsluttetHendelseDto::class, name = "OPPFOLGING_AVSLUTTET")
+)
+abstract class GjeldendeOppfolgingsperiodeDto(
+    val hendelseType: HendelseType,
+    val producerTimestamp: ZonedDateTime = ZonedDateTime.now(),
+    val producerAppName: HendelseProducerAppName = HendelseProducerAppName.VEILARBOPPFOLGING,
+)
+
+enum class HendelseProducerAppName {
+    VEILARBOPPFOLGING,
+    AO_OPPFOLGINGSKONTOR
+}
+
+data class GjeldendeOppfolgingsperiode(
+    val oppfolgingsperiodeId: UUID,
+    val startTidspunkt: ZonedDateTime,
+    val sluttTidspunkt: ZonedDateTime?,
+    val aktorId: String,
+    val ident: String,
+    val kontorId: String?,
+    val kontorNavn: String?,
 )
