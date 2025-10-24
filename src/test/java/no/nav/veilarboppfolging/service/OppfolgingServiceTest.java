3db1cd175b30f55f333aa9dc0a4f0f406fdbd16b
sigurdgroneng <sigurd.groneng@nav.no>
2025-10-24 07:48:44 +0200

    Add missing mocks in tests

--- src/test/java/no/nav/veilarboppfolging/service/OppfolgingServiceTest.java
+++ src/test/java/no/nav/veilarboppfolging/service/OppfolgingServiceTest.java
@@ -80,6 +80,7 @@ public class OppfolgingServiceTest extends IsolatedDatabaseTest {
     private OppfolgingService oppfolgingService;
     private StartOppfolgingService startOppfolgingService;
     private BigQueryClient bigQueryClient = mock(BigQueryClient.class);
+    private OppfolgingsperiodeEndretService oppfolgingsperiodeEndretService = mock(OppfolgingsperiodeEndretService.class);
 
     @Before
     public void setup() {
@@ -106,6 +107,7 @@ public class OppfolgingServiceTest extends IsolatedDatabaseTest {
                  transactor,
                 arenaYtelserService,
                 bigQueryClient,
+                oppfolgingsperiodeEndretService,
                 "https://test.nav.no"
                 );
 
