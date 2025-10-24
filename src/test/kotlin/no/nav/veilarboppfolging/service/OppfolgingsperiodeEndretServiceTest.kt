e604e9c698c36e06c0816d1f0dbd4b506d9b857e
Giil <mads.lee.giil@nav.no>
2025-10-24 10:52:32 +0200

    Legg til flere test caser

--- src/test/kotlin/no/nav/veilarboppfolging/service/OppfolgingsperiodeEndretServiceTest.kt
+++ src/test/kotlin/no/nav/veilarboppfolging/service/OppfolgingsperiodeEndretServiceTest.kt
@@ -1,6 +1,8 @@
 package no.nav.veilarboppfolging.service
 
 import no.nav.common.client.aktoroppslag.AktorOppslagClient
+import no.nav.common.types.identer.AktorId
+import no.nav.common.types.identer.Fnr
 import no.nav.veilarboppfolging.kafka.OppfolgingskontorMelding
 import no.nav.veilarboppfolging.kafka.Tilordningstype
 import no.nav.veilarboppfolging.oppfolgingsbruker.StartetAvType
@@ -8,20 +10,11 @@ import no.nav.veilarboppfolging.repository.OppfolgingsPeriodeRepository
 import no.nav.veilarboppfolging.repository.entity.OppfolgingsperiodeEntity
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
-import org.junit.runner.RunWith
+import org.mockito.kotlin.*
 import java.time.ZonedDateTime
 import java.util.*
-import org.mockito.kotlin.mock
-import org.mockito.kotlin.whenever
-import org.mockito.kotlin.verify
-import org.mockito.kotlin.any
-import org.mockito.kotlin.argumentCaptor
-import org.mockito.kotlin.doNothing
-import org.mockito.kotlin.doThrow
-import org.mockito.kotlin.verifyNoInteractions
 
 
-//@RunWith(MockitoJUnitRunner::class)
 class OppfolgingsperiodeEndretServiceTest {
 
     val oppfolgingsPeriodeRepository: OppfolgingsPeriodeRepository = mock()
@@ -31,14 +24,14 @@ class OppfolgingsperiodeEndretServiceTest {
     private val oppfolgingsperiodeEndretService = OppfolgingsperiodeEndretService(oppfolgingsPeriodeRepository, kafkaProducerService, aktorOppslagClient)
 
     @Test
-    fun `skal ignorere tom melding`() {
+    fun `skal ignorere tom oppfolgingskontormelding`() {
         oppfolgingsperiodeEndretService.håndterOppfolgingskontorMelding(null)
 
         verifyNoInteractions(kafkaProducerService, oppfolgingsPeriodeRepository, aktorOppslagClient)
     }
 
     @Test
-    fun `Skal sende melding om OPPFOLGING_STARTET når  oppfolgingskontormelding mottas med tilordningstype KONTOR_VED_OPPFOLGINGSPERIODE_START`() {
+    fun `Skal sende melding med type OPPFOLGING_STARTET når oppfolgingskontormelding mottas med tilordningstype KONTOR_VED_OPPFOLGINGSPERIODE_START`() {
         val aktorId = "1111111"
         val oppfolgingsperiode = oppfolgingsperiode(aktorId)
         whenever(oppfolgingsPeriodeRepository.hentOppfolgingsperiode(oppfolgingsperiode.uuid.toString())).thenReturn(Optional.of(oppfolgingsperiode))
@@ -55,6 +48,51 @@ class OppfolgingsperiodeEndretServiceTest {
         assertThat(oppfolgingsperiodeMelding.oppfolgingsperiodeUuid).isEqualTo(oppfolgingskontorMelding.oppfolgingsperiodeId)
         assertThat(oppfolgingsperiodeMelding.kontorId).isEqualTo(oppfolgingskontorMelding.kontorId)
         assertThat(oppfolgingsperiodeMelding.kontorNavn).isEqualTo(oppfolgingskontorMelding.kontorNavn)
+        assertThat(oppfolgingsperiodeMelding.startTidspunkt).isEqualTo(oppfolgingsperiode.startDato)
+    }
+
+    @Test
+    fun `Skal sende melding med type ARBEIDSOPPFOLGINGSKONTOR_ENDRET når oppfolgingskontormelding mottas med tilordningstype ENDRET_KONTOR`() {
+        val aktorId = "1111111"
+        val oppfolgingsperiode = oppfolgingsperiode(aktorId)
+        whenever(oppfolgingsPeriodeRepository.hentOppfolgingsperiode(oppfolgingsperiode.uuid.toString())).thenReturn(Optional.of(oppfolgingsperiode))
+        val oppfolgingskontorMelding = oppfolgingskontorMelding(aktorId = aktorId, tilordningstype = Tilordningstype.ENDRET_KONTOR, oppfolgingsperiodeId = oppfolgingsperiode.uuid)
+
+        oppfolgingsperiodeEndretService.håndterOppfolgingskontorMelding(oppfolgingskontorMelding)
+
+        val captor = argumentCaptor<SisteOppfolgingsperiodeDto>()
+        verify(kafkaProducerService).publiserOppfolgingsperiodeMedKontor(captor.capture())
+        val oppfolgingsperiodeMelding = captor.firstValue as GjeldendeOppfolgingsperiode
+        assertThat(oppfolgingsperiodeMelding.sisteEndringsType).isEqualTo(SisteEndringsType.ARBEIDSOPPFOLGINGSKONTOR_ENDRET)
+        assertThat(oppfolgingsperiodeMelding.aktorId).isEqualTo(oppfolgingskontorMelding.aktorId)
+        assertThat(oppfolgingsperiodeMelding.ident).isEqualTo(oppfolgingskontorMelding.ident)
+        assertThat(oppfolgingsperiodeMelding.oppfolgingsperiodeUuid).isEqualTo(oppfolgingskontorMelding.oppfolgingsperiodeId)
+        assertThat(oppfolgingsperiodeMelding.kontorId).isEqualTo(oppfolgingskontorMelding.kontorId)
+        assertThat(oppfolgingsperiodeMelding.kontorNavn).isEqualTo(oppfolgingskontorMelding.kontorNavn)
+        assertThat(oppfolgingsperiodeMelding.startTidspunkt).isEqualTo(oppfolgingsperiode.startDato)
+    }
+
+    @Test
+    fun `Skal sende melding med type OPPFOLGING_AVSLUTTET når oppfølgingsperiode avsluttes`() {
+        val aktorId = "1111111"
+        val fnr = Fnr("01015054321")
+        val startTidspunkt = ZonedDateTime.now().minusDays(10)
+        val sluttTidspunkt = ZonedDateTime.now()
+        val oppfolgingsperiode = oppfolgingsperiode(aktorId, startTidspunkt, sluttTidspunkt)
+        whenever(aktorOppslagClient.hentFnr(AktorId.of(aktorId))).thenReturn(fnr)
+
+
+        oppfolgingsperiodeEndretService.håndterOppfolgingAvsluttet(oppfolgingsperiode)
+
+        val captor = argumentCaptor<SisteOppfolgingsperiodeDto>()
+        verify(kafkaProducerService).publiserOppfolgingsperiodeMedKontor(captor.capture())
+        val oppfolgingsperiodeMelding = captor.firstValue as AvsluttetOppfolgingsperiode
+        assertThat(oppfolgingsperiodeMelding.sisteEndringsType).isEqualTo(SisteEndringsType.OPPFOLGING_AVSLUTTET)
+        assertThat(oppfolgingsperiodeMelding.aktorId).isEqualTo(oppfolgingsperiode.aktorId)
+        assertThat(oppfolgingsperiodeMelding.ident).isEqualTo(fnr.get())
+        assertThat(oppfolgingsperiodeMelding.oppfolgingsperiodeUuid).isEqualTo(oppfolgingsperiode.uuid)
+        assertThat(oppfolgingsperiodeMelding.startTidspunkt).isEqualTo(oppfolgingsperiode.startDato)
+        assertThat(oppfolgingsperiodeMelding.sluttTidspunkt).isEqualTo(oppfolgingsperiode.sluttDato)
     }
 
     private fun oppfolgingsperiode(aktorId: String, oppfolgingsperiodeStart: ZonedDateTime = ZonedDateTime.now(), oppfolgingsperiodeSlutt: ZonedDateTime? = null): OppfolgingsperiodeEntity {
