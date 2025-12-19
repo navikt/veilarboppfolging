51c0eb9a14bfcc7cc87310b96194bf89f66d19e1
Erik.Bosnes <erik.bosnes@nav.no>
2025-12-19 10:45:01 +0100

    Cleanup

--- src/main/resources/db/migration/V16__ao_kontor_endre_pk_til_oppfolgingsperiodeid.sql
+++ src/main/resources/db/migration/V16__ao_kontor_endre_pk_til_oppfolgingsperiodeid.sql
@@ -1,6 +1,6 @@
-ALTER TABLE veilarboppfolging.ao_kontor
+ALTER TABLE ao_kontor
     DROP CONSTRAINT ao_kontor_pkey;
 
-ALTER TABLE veilarboppfolging.ao_kontor
+ALTER TABLE ao_kontor
     ADD CONSTRAINT ao_kontor_oppfolgingsperiode_pk
         PRIMARY KEY (oppfolgingsperiode_id);
