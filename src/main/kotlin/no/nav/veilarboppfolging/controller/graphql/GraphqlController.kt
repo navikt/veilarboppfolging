be0e435a03d3a0a72922811bf7b5fb77dd3bf392
Mads.Lee.Giil <mads.lee.giil@nav.no>
2026-03-09 12:49:44 +0100

    Ta høyde for at poao-tilgang skal oppdatere grensesnitt for tilgangssjekk

--- src/main/kotlin/no/nav/veilarboppfolging/controller/graphql/GraphqlController.kt
+++ src/main/kotlin/no/nav/veilarboppfolging/controller/graphql/GraphqlController.kt
@@ -384,19 +384,47 @@ class GraphqlController(
         if (this is AktorId) return aktorOppslagClient.hentFnr(this)
         return Fnr(this.get())
     }
-}
 
-fun Decision.Deny.tryToFindDenyReason(): TilgangResultat {
-    if (this.reason != "MANGLER_TILGANG_TIL_AD_GRUPPE") return TilgangResultat.IKKE_TILGANG_ENHET
-    return when {
-        this.message.contains(AdGruppeNavn.STRENGT_FORTROLIG_ADRESSE) -> return TilgangResultat.IKKE_TILGANG_STRENGT_FORTROLIG_ADRESSE
-        this.message.contains(AdGruppeNavn.FORTROLIG_ADRESSE) -> return TilgangResultat.IKKE_TILGANG_FORTROLIG_ADRESSE
-        this.message.contains(AdGruppeNavn.EGNE_ANSATTE) -> return TilgangResultat.IKKE_TILGANG_EGNE_ANSATTE
-        this.message.contains(AdGruppeNavn.MODIA_GENERELL) -> return TilgangResultat.IKKE_TILGANG_MODIA
-        this.message.contains(AdGruppeNavn.MODIA_OPPFOLGING) -> return TilgangResultat.IKKE_TILGANG_MODIA
-        else -> TilgangResultat.IKKE_TILGANG_ENHET
+    fun Decision.Deny.tryToFindDenyReason(): TilgangResultat {
+        val denyReason = runCatching {
+            DecisionDenyReason.valueOf(this.reason)
+        }.getOrNull()
+
+        return when (denyReason) {
+            DecisionDenyReason.POLICY_IKKE_IMPLEMENTERT,
+            DecisionDenyReason.EKSTERN_BRUKER_HAR_IKKE_TILGANG,
+            DecisionDenyReason.UKLAR_TILGANG_MANGLENDE_INFORMASJON -> TilgangResultat.IKKE_TILGANG_ENHET.also {
+                logger.warn("Fikk uforventet svar om ikke tilgang, skal egentlig ikke skje. Svak programmering – vi må ha gjort noe feil: ${this.reason}, ${this.message}")
+            }
+            DecisionDenyReason.IKKE_TILGANG_TIL_NAV_ENHET -> TilgangResultat.IKKE_TILGANG_ENHET
+            DecisionDenyReason.IKKE_TILGANG_TIL_FORTROLIG_BRUKER ->  TilgangResultat.IKKE_TILGANG_FORTROLIG_ADRESSE
+            DecisionDenyReason.IKKE_TILGANG_TIL_STRENGT_FORTROLIG_BRUKER,
+            DecisionDenyReason.IKKE_TILGANG_TIL_STRENGT_FORTROLIG_UTLAND_BRUKER-> TilgangResultat.IKKE_TILGANG_STRENGT_FORTROLIG_ADRESSE
+            DecisionDenyReason.IKKE_TILGANG_TIL_SKJERMET_PERSON -> TilgangResultat.IKKE_TILGANG_EGNE_ANSATTE
+            DecisionDenyReason.MANGLER_TILGANG_TIL_AD_GRUPPE, null -> when {
+                this.message.contains(AdGruppeNavn.STRENGT_FORTROLIG_ADRESSE) -> return TilgangResultat.IKKE_TILGANG_STRENGT_FORTROLIG_ADRESSE
+                this.message.contains(AdGruppeNavn.FORTROLIG_ADRESSE) -> return TilgangResultat.IKKE_TILGANG_FORTROLIG_ADRESSE
+                this.message.contains(AdGruppeNavn.EGNE_ANSATTE) -> return TilgangResultat.IKKE_TILGANG_EGNE_ANSATTE
+                this.message.contains(AdGruppeNavn.MODIA_GENERELL) -> return TilgangResultat.IKKE_TILGANG_MODIA
+                this.message.contains(AdGruppeNavn.MODIA_OPPFOLGING) -> return TilgangResultat.IKKE_TILGANG_MODIA
+                else -> TilgangResultat.IKKE_TILGANG_ENHET
+            }
+        }
     }
 }
+
+enum class DecisionDenyReason {
+    MANGLER_TILGANG_TIL_AD_GRUPPE,
+    POLICY_IKKE_IMPLEMENTERT,
+    IKKE_TILGANG_TIL_NAV_ENHET,
+    UKLAR_TILGANG_MANGLENDE_INFORMASJON,
+    EKSTERN_BRUKER_HAR_IKKE_TILGANG,
+    IKKE_TILGANG_TIL_FORTROLIG_BRUKER,
+    IKKE_TILGANG_TIL_STRENGT_FORTROLIG_BRUKER,
+    IKKE_TILGANG_TIL_STRENGT_FORTROLIG_UTLAND_BRUKER,
+    IKKE_TILGANG_TIL_SKJERMET_PERSON,
+}
+
 
 object AdGruppeNavn {
     const val STRENGT_FORTROLIG_ADRESSE = "0000-GA-Strengt_Fortrolig_Adresse"
