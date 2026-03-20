19bc47613f5b7b62ade08fccc46b7334bcfcb6b8
Tia Firing <tia.firing@nav.no>
2026-03-20 14:01:41 +0100

    flytter api

--- src/main/java/no/nav/veilarboppfolging/controller/v2/AdminV2Controller.java
+++ src/main/java/no/nav/veilarboppfolging/controller/v2/AdminV2Controller.java
@@ -1,20 +1,14 @@
 package no.nav.veilarboppfolging.controller.v2;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
-import no.nav.common.auth.context.AuthContextHolder;
-import no.nav.common.auth.context.UserRole;
 import no.nav.common.job.JobRunner;
 import no.nav.veilarboppfolging.ForbiddenException;
-import no.nav.veilarboppfolging.UnauthorizedException;
-import no.nav.veilarboppfolging.controller.v2.request.RepubliserVeilederRequest;
 import no.nav.veilarboppfolging.service.AuthService;
 import no.nav.veilarboppfolging.service.KafkaRepubliseringService;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/v2/admin")
@@ -35,15 +29,6 @@ public class AdminV2Controller {
     public String republiserTilordnetVeileder() {
         sjekkTilgangTilAdmin();
         return JobRunner.runAsync("republiser-tilordnet-veileder", kafkaRepubliseringService::republiserTilordnetVeileder);
-    }
-
-    @PostMapping("/republiser/tilordnet-veileder/utvalg")
-    public String republiserTilordnetVeileder(@RequestBody RepubliserVeilederRequest republiserVeilederRequest) {
-        sjekkTilgangTilAdmin();
-        return JobRunner.runAsync(
-                "republiser-tilordnet-veileder-gitte-aktorider",
-                () -> kafkaRepubliseringService.republiserTilordnetVeileder(republiserVeilederRequest.aktorIder())
-        );
     }
 
     private void sjekkTilgangTilAdmin() {
