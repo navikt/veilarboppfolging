f8d653cf7cb8506963b0b429fa8fdd150f1a634b
sigurdgroneng <sigurd.groneng@nav.no>
2026-04-09 16:45:17 +0200

    Make sonar happy

--- src/main/kotlin/no/nav/veilarboppfolging/oppfolgingsbruker/inngang/KanStarteOppfolgingEksterneDto.kt
+++ src/main/kotlin/no/nav/veilarboppfolging/oppfolgingsbruker/inngang/KanStarteOppfolgingEksterneDto.kt
@@ -22,13 +22,13 @@ enum class KanStarteOppfolgingEksterneDto {
                 is UKJENT_STATUS_FOLKEREGISTERET -> return UKJENT_STATUS_FOLKEREGISTERET
                 is FREG_STATUS_KREVER_MANUELL_GODKJENNING_PGA_DNUMMER_IKKE_EOS_GBR -> return IKKE_LOVLIG_OPPHOLD
                 is FREG_STATUS_KREVER_MANUELL_GODKJENNING_PGA_IKKE_BOSATT -> return IKKE_LOVLIG_OPPHOLD
-                is FREG_STATUS_OK -> {}
-            }
-
-            return when (erBrukerUnderOppfolging.value) {
-                is ALLEREDE_UNDER_OPPFOLGING -> ALLEREDE_UNDER_OPPFOLGING
-                is ALLEREDE_UNDER_OPPFOLGING_MEN_INAKTIVERT -> ALLEREDE_UNDER_OPPFOLGING_MEN_INAKTIVERT
-                is OPPFOLGING_OK -> JA
+                is FREG_STATUS_OK -> {
+                    return when (erBrukerUnderOppfolging.value) {
+                        is ALLEREDE_UNDER_OPPFOLGING -> ALLEREDE_UNDER_OPPFOLGING
+                        is ALLEREDE_UNDER_OPPFOLGING_MEN_INAKTIVERT -> ALLEREDE_UNDER_OPPFOLGING_MEN_INAKTIVERT
+                        is OPPFOLGING_OK -> JA
+                    }
+                }
             }
         }
     }
