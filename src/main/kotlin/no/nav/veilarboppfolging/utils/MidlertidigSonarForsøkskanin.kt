319a3035eac4e7c5ad4cffdca39fa49c1bd19c08
Mads.Lee.Giil <mads.lee.giil@nav.no>
2026-05-11 10:43:10 +0200

    Korriger forsøkskanin

--- src/main/kotlin/no/nav/veilarboppfolging/utils/MidlertidigSonarForsøkskanin.kt
+++ src/main/kotlin/no/nav/veilarboppfolging/utils/MidlertidigSonarForsøkskanin.kt
@@ -2,6 +2,6 @@ package no.nav.veilarboppfolging.utils
 
 class MidlertidigSonarForsøkskanin {
 
-    fun leggSammen(tall1: Int, tall2: Int) = tall1
+    fun leggSammen(tall1: Int, tall2: Int) = tall1 + tall2
 
 }
