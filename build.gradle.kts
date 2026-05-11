fc8e6090dc557d1140a5dd638cc2c9c255f6de10
Mads.Lee.Giil <mads.lee.giil@nav.no>
2026-05-11 11:54:00 +0200

    Oppdater kode til å reflektere at det var Kotlin-binærfiler som måtte spesifiseres

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,10 +45,6 @@ tasks.jacocoTestReport {
         html.required.set(true)
     }
 }
-//
-//jacoco {
-//    toolVersion = "0.8.14"
-//}
 
 tasks.sonar {
     dependsOn(tasks.jacocoTestReport)
@@ -144,8 +140,7 @@ sonarqube {
         property("sonar.projectKey", "navikt_veilarboppfolging")
         property("sonar.organization", "navikt")
         property("sonar.host.url", "https://sonarcloud.io")
-        property("sonar.java.binaries", "${project.layout.buildDirectory.get().asFile}/classes/java/main")
-//        property("sonar.java.binaries", "${project.layout.buildDirectory.get().asFile}/classes/java/main,${project.layout.buildDirectory.get().asFile}/classes/kotlin/main")
+        property("sonar.java.binaries", "${project.layout.buildDirectory.get().asFile}/classes/java/main,${project.layout.buildDirectory.get().asFile}/classes/kotlin/main")
     }
 }
 
