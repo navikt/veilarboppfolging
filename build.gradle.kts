79c8eaafedab8072aac0a4060d73d882b8e14f65
Tia Firing <tia.firing@nav.no>
2026-05-19 07:11:47 +0200

    tester ny kafkametrikk-fiks

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 val kotlinVersion = "2.3.21"
 val flywayVersion = "12.0.3"
-val commonVersion = "4.2026.05.15_08.54-961583f8b296"
+val commonVersion = "4.2026.05.19_05.04-98f1c9358275-beta"
 val ptoSchemaVersion = "1.2025.09.29_11.36-6e568fa24c23"
 val poaoTilgangVersion = "4.2026.05.11_07.01-54ab6eae4dde"
 val wiremockVersion = "3.13.2"
