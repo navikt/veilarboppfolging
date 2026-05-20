cffdcf2bca4b688425f672ee6ba1e6591f219526
Mads.Lee.Giil <mads.lee.giil@nav.no>
2026-05-20 13:43:36 +0200

    Test om person har blitt sykmeldt uten arbeidsgiver

--- src/test/kotlin/no/nav/veilarboppfolging/kafka/TestUtils.kt
+++ src/test/kotlin/no/nav/veilarboppfolging/kafka/TestUtils.kt
@@ -1,13 +1,18 @@
 package no.nav.veilarboppfolging.kafka
 
+import no.nav.common.types.identer.EnhetId
+import no.nav.common.types.identer.Fnr
 import no.nav.pto_schema.enums.arena.Formidlingsgruppe
 import no.nav.pto_schema.enums.arena.Hovedmaal
 import no.nav.pto_schema.enums.arena.Kvalifiseringsgruppe
 import no.nav.pto_schema.enums.arena.Rettighetsgruppe
 import no.nav.pto_schema.enums.arena.SikkerhetstiltakType
 import no.nav.pto_schema.kafka.json.topic.onprem.EndringPaaOppfoelgingsBrukerV2
+import no.nav.veilarboppfolging.oppfolgingsbruker.arena.LocalArenaOppfolging
+import no.nav.veilarboppfolging.repository.entity.OppfolgingEntity
 import java.time.LocalDate
 import java.time.ZonedDateTime
+import java.util.Optional
 
 object TestUtils {
 
@@ -38,4 +43,28 @@ object TestUtils {
             ZonedDateTime.now()
         )
     }
+
+    fun oppfølgingEntity(
+        aktorId: String,
+        veilederId: String = "A111111",
+        underOppfolging: Boolean = true,
+        localArenaOppfølging: LocalArenaOppfolging = localArenaOppfolging()
+    ) =
+        OppfolgingEntity()
+            .setAktorId(aktorId)
+            .setVeilederId(veilederId)
+            .setUnderOppfolging(underOppfolging)
+            .setGjeldendeManuellStatusId(1)
+            .setGjeldendeMaalId(1)
+            .setGjeldendeKvpId(0)
+            .setLocalArenaOppfolging(Optional.of(localArenaOppfølging))
+
+    fun localArenaOppfolging(kvalifiseringsgruppe: Kvalifiseringsgruppe = Kvalifiseringsgruppe.IVURD, formidlingsgruppe: Formidlingsgruppe = Formidlingsgruppe.ARBS) =
+        LocalArenaOppfolging(
+            hovedmaal = Hovedmaal.BEHOLDEA,
+            kvalifiseringsgruppe = kvalifiseringsgruppe,
+            formidlingsgruppe = formidlingsgruppe,
+            oppfolgingsenhet = EnhetId("8686"),
+            iservFraDato = null
+        )
 }
---
+++ src/test/kotlin/no/nav/veilarboppfolging/service/ResolveEndringPaaOppfolgingsbrukerEventTest.kt
@@ -0,0 +1,51 @@
+package no.nav.veilarboppfolging.service
+
+import no.nav.pto_schema.enums.arena.Formidlingsgruppe.IARBS
+import no.nav.pto_schema.enums.arena.Kvalifiseringsgruppe.IVURD
+import no.nav.pto_schema.enums.arena.Kvalifiseringsgruppe.VURDU
+import no.nav.veilarboppfolging.ident.randomAktorId
+import no.nav.veilarboppfolging.ident.randomFnr
+import no.nav.veilarboppfolging.kafka.TestUtils.localArenaOppfolging
+import no.nav.veilarboppfolging.kafka.TestUtils.oppfølgingEntity
+import no.nav.veilarboppfolging.kafka.TestUtils.oppfølgingsBrukerEndret
+import no.nav.veilarboppfolging.oppfolgingsbruker.arena.EndringPaaOppfolgingsBruker
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import java.util.Optional
+
+class ResolveEndringPaaOppfolgingsbrukerEventTest {
+
+    @Test
+    fun `Endring på oppfølgingsbruker som var sykmeldt uten arbeidsgiver og fortsatt er sykmeldt uten arbeidsgiver er IrrelevantEndring`() {
+        val aktorId = randomAktorId()
+        val nåværendeOppfølgingstatus =
+            oppfølgingEntity(aktorId = aktorId.get(), localArenaOppfølging = localArenaOppfolging(kvalifiseringsgruppe = VURDU, formidlingsgruppe = IARBS))
+        val oppfolgingsbrukerEndret = oppfølgingsBrukerEndret(fnr = randomFnr().get(), formidlingsgruppe = IARBS, kvalifiseringsgruppe = VURDU)
+
+        val endring = resolveEndringPaaOppfolgingsbrukerEvent(
+            endringPaaOppfolgingsBruker = EndringPaaOppfolgingsBruker.from(oppfolgingsbrukerEndret, aktorId),
+            nåværendeOppfolgingsstatus = nåværendeOppfølgingstatus,
+            getKanReaktiveresIArena = { Optional.of(true) },
+            kanAvsluttes = { OppfolgingService.KanAvslutteMedBegrunnelse(true, "Begrunnelse") }
+        )
+
+        assertThat(endring).isInstanceOf(IrrelevantEndring::class.java)
+    }
+
+    @Test
+    fun `Endring på oppfølgingsbruker som har blitt sykmeldt uten arbeidsgiver er BleSykmeldtUtenArbeidsgiver`() {
+        val aktorId = randomAktorId()
+        val nåværendeOppfølgingstatus =
+            oppfølgingEntity(aktorId = aktorId.get(), localArenaOppfølging = localArenaOppfolging(kvalifiseringsgruppe = IVURD, formidlingsgruppe = IARBS))
+        val oppfolgingsbrukerEndret = oppfølgingsBrukerEndret(fnr = randomFnr().get(), formidlingsgruppe = IARBS, kvalifiseringsgruppe = VURDU)
+
+        val endring = resolveEndringPaaOppfolgingsbrukerEvent(
+            endringPaaOppfolgingsBruker = EndringPaaOppfolgingsBruker.from(oppfolgingsbrukerEndret, aktorId),
+            nåværendeOppfolgingsstatus = nåværendeOppfølgingstatus,
+            getKanReaktiveresIArena = { Optional.of(true) },
+            kanAvsluttes = { OppfolgingService.KanAvslutteMedBegrunnelse(true, "Begrunnelse") }
+        )
+
+        assertThat(endring).isInstanceOf(BleSykmeldtUtenArbeidsgiver::class.java)
+    }
+}
