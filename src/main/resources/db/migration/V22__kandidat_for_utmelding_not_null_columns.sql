e305b32b92c4fdc270368da130c77df964b01511
Mads.Lee.Giil <mads.lee.giil@nav.no>
2026-06-08 14:50:54 +0200

    Gjør kolonner i utmeldingskandidattabellen not-nullable

---
+++ src/main/resources/db/migration/V22__kandidat_for_utmelding_not_null_columns.sql
@@ -0,0 +1,3 @@
+ALTER TABLE kandidat_for_utmelding ALTER COLUMN avsluttet_av SET NOT NULL;
+ALTER TABLE kandidat_for_utmelding ALTER COLUMN kilde SET NOT NULL;
+ALTER TABLE kandidat_for_utmelding ALTER COLUMN fnr SET NOT NULL;
