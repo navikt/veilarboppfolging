bb7ae1a8acccf2d3ecff9d8dff2867e0f918be30
sigurdgroneng <sigurd.groneng@nav.no>
2026-07-08 11:16:22 +0200

    Cleanup

--- src/test/java/no/nav/veilarboppfolging/service/OppfolgingServiceTest2.java
+++ src/test/java/no/nav/veilarboppfolging/service/OppfolgingServiceTest2.java
@@ -1,13 +1,8 @@
 package no.nav.veilarboppfolging.service;
 
-import no.nav.common.auth.context.AuthContext;
-import no.nav.common.auth.context.AuthContextHolder;
-import no.nav.common.kafka.producer.feilhandtering.KafkaProducerRecordStorage;
 import no.nav.common.types.identer.AktorId;
 import no.nav.common.types.identer.Fnr;
 import no.nav.common.types.identer.NavIdent;
-import no.nav.common.utils.fn.UnsafeRunnable;
-import no.nav.common.utils.fn.UnsafeSupplier;
 import no.nav.pto_schema.enums.arena.Formidlingsgruppe;
 import no.nav.pto_schema.enums.arena.Kvalifiseringsgruppe;
 import no.nav.veilarboppfolging.client.digdir_krr.DigdirClient;
