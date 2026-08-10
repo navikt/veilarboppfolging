5e1abe684242edbfc49dfe805521fc23acaf5fae
Sondre Larsen Ovrid <sondre.larsen.ovrid@nav.no>
2026-08-10 13:06:05 +0200

    Legg til funksjon for å hente oppfølgingsenhet med AktørID

--- src/main/kotlin/no/nav/veilarboppfolging/service/ArbeidsoppfolgingsKontorService.kt
+++ src/main/kotlin/no/nav/veilarboppfolging/service/ArbeidsoppfolgingsKontorService.kt
@@ -1,5 +1,7 @@
 package no.nav.veilarboppfolging.service
 
+import no.nav.common.client.aktoroppslag.AktorOppslagClient
+import no.nav.common.types.identer.AktorId
 import no.nav.common.types.identer.EnhetId
 import no.nav.common.types.identer.Fnr
 import no.nav.veilarboppfolging.repository.ArbeidsoppfolgingskontorRepository
@@ -8,8 +10,15 @@ import org.springframework.stereotype.Service
 @Service
 class ArbeidsoppfolgingsKontorService(
     val arbeidsoppfolgingskontorRepository: ArbeidsoppfolgingskontorRepository,
+    val aktorOppslagClient: AktorOppslagClient
+
 ) {
     fun hentOppfolgingsEnhetId(fnr: Fnr): EnhetId? {
-        return arbeidsoppfolgingskontorRepository.hentEnhet(fnr)
+        val aktorId = aktorOppslagClient.hentAktorId(fnr)
+        return hentOppfolgingsEnhetId(aktorId)
+    }
+
+    fun hentOppfolgingsEnhetId(aktorId: AktorId): EnhetId? {
+        return arbeidsoppfolgingskontorRepository.hentEnhet(aktorId)
     }
 }
