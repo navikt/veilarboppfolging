8e4fa530432e44ed0520eaaef00be80d57843fa2
sigurdgroneng <sigurd.groneng@nav.no>
2026-08-11 11:24:47 +0200

    Legg til mock for testing

--- src/test/java/no/nav/veilarboppfolging/service/OppfolgingServiceTest.java
+++ src/test/java/no/nav/veilarboppfolging/service/OppfolgingServiceTest.java
@@ -5,6 +5,8 @@ import no.nav.common.types.identer.AktorId;
 import no.nav.common.types.identer.EnhetId;
 import no.nav.common.types.identer.Fnr;
 import no.nav.common.types.identer.NavIdent;
+import no.nav.poao_tilgang.client.Decision;
+import no.nav.poao_tilgang.client.TilgangType;
 import no.nav.pto_schema.enums.arena.Formidlingsgruppe;
 import no.nav.pto_schema.enums.arena.Kvalifiseringsgruppe;
 import no.nav.veilarboppfolging.ForbiddenException;
@@ -335,6 +337,7 @@ public class OppfolgingServiceTest extends IsolatedDatabaseTest {
     @Test
     public void harVeilederTilgang__medEnhetTilgangTilBrukersEnhet() {
         when(authService.harTilgangTilEnhet(any())).thenReturn(true);
+        when(authService.evaluerNavAnsattTilgangTilBruker(fnr, TilgangType.LESE)).thenReturn(Decision.Permit.INSTANCE);
         when(arbeidsoppfolgingsKontorService.hentOppfolgingsEnhetId(fnr)).thenReturn(EnhetId.of(ENHET));
 
         var tilgang = oppfolgingService.harVeilederTilgangTilBrukersEnhet(fnr);
@@ -345,6 +348,7 @@ public class OppfolgingServiceTest extends IsolatedDatabaseTest {
     @Test
     public void harVeilederTilgang__utenEnhetTilgangTilBrukersEnhet() {
         when(arbeidsoppfolgingsKontorService.hentOppfolgingsEnhetId(fnr)).thenReturn(EnhetId.of(ENHET));
+        when(authService.evaluerNavAnsattTilgangTilBruker(fnr, TilgangType.LESE)).thenReturn(Decision.Permit.INSTANCE);
 
         VeilederTilgang veilederIkkeTilgang = oppfolgingService.harVeilederTilgangTilBrukersEnhet(fnr);
         assertFalse(veilederIkkeTilgang.getTilgangTilBrukersKontor());
@@ -353,6 +357,7 @@ public class OppfolgingServiceTest extends IsolatedDatabaseTest {
     @Test
     public void harVeilederTilgang__skal_ikke_ha_tilgang_TilBrukersEnhet_nar_bruker_ikke_har_enhet() {
         when(arbeidsoppfolgingsKontorService.hentOppfolgingsEnhetId(fnr)).thenReturn(null);
+        when(authService.evaluerNavAnsattTilgangTilBruker(fnr, TilgangType.LESE)).thenReturn(Decision.Permit.INSTANCE);
 
         VeilederTilgang veilederIkkeTilgang = oppfolgingService.harVeilederTilgangTilBrukersEnhet(fnr);
         assertFalse(veilederIkkeTilgang.getTilgangTilBrukersKontor());
