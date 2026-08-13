3eb6b574f2e6650d325e52a3e54bbcfd3900b376
johannetronstad <johanne.tronstad@nav.no>
2026-08-13 12:13:44 +0200

    fixks type på oppfølgingsperiode

--- src/main/resources/db/migration/V35__utmeldingskandidat_v2.sql
+++ src/main/resources/db/migration/V35__utmeldingskandidat_v2.sql
@@ -5,7 +5,7 @@ create table kandidater_for_utmelding_hendelser (
     utfort_av varchar, -- ident til veileder eller bruker, mangler i tidlige data
     utfort_av_type varchar not null, -- VEILEDER, BRUKAR
     kilde varchar not null, -- Feks arbeidssøkerregisteret
-    oppfolgingsperiode_uuid uuid references oppfolgingsperiode(uuid) not null,
+    oppfolgingsperiode_uuid char(36) references oppfolgingsperiode(uuid) not null,
     created_at timestamp default current_timestamp not null
 );
 
@@ -13,7 +13,7 @@ create index hendelse_oppfolgingsperiode_idx ON kandidater_for_utmelding_hendels
 
 create table kandidater_for_utmelding (
     forlenget_til timestamp,
-    oppfolgingsperiode_uuid uuid references oppfolgingsperiode(uuid) not null primary key,
+    oppfolgingsperiode_uuid char(36) references oppfolgingsperiode(uuid) not null primary key,
     siste_utmeldingshendelse_id uuid references kandidater_for_utmelding_hendelser(utmeldingshendelse_id) not null,
     created_at timestamp default current_timestamp not null,
     updated_at timestamp default current_timestamp not null
@@ -23,7 +23,7 @@ create index kandidat_oppfolgingsperiode_idx ON kandidater_for_utmelding(oppfolg
 
 create table filterkategori_id_mapping (
     kategori varchar not null,
-    oppfolgingsperiode_id uuid references oppfolgingsperiode(uuid) not null,
+    oppfolgingsperiode_id char(36) references oppfolgingsperiode(uuid) not null,
     filterkategori_person_id uuid not null,
     PRIMARY KEY (oppfolgingsperiode_id, kategori)
 );
