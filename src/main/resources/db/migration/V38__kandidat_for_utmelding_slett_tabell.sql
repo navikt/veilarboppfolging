09e90976cf7daa879f3c603670adb50ef9e63006
Kjell-Magne.Larsen <kjell-magne.larsen@nav.no>
2026-08-25 14:09:09 +0200

    slett tabell kandidat_for_utmelding og fk constraints dersom de eksisterer

--- src/main/resources/db/migration/V38__kandidat_for_utmelding_slett_tabell.sql
+++ src/main/resources/db/migration/V38__kandidat_for_utmelding_slett_tabell.sql
@@ -1,2 +1,2 @@
-ALTER TABLE kandidat_for_utmelding DROP CONSTRAINT oppfolgingsperiode_uuid_fk;
-DROP TABLE kandidat_for_utmelding;
+ALTER TABLE IF EXISTS kandidat_for_utmelding DROP CONSTRAINT IF EXISTS oppfolgingsperiode_uuid_fk;
+DROP TABLE IF EXISTS kandidat_for_utmelding;
