46d1aad45ca50dbd139149117385b52d36baa684
Tia Firing <tia.firing@nav.no>
2026-08-27 09:54:34 +0200

    tar i bruk nytt api for å sjekke om bruker mottar eller har søkt om AAP

--- src/test/kotlin/no/nav/veilarboppfolging/client/aap/AapClientTest.kt
+++ src/test/kotlin/no/nav/veilarboppfolging/client/aap/AapClientTest.kt
@@ -4,30 +4,39 @@ import com.github.tomakehurst.wiremock.client.WireMock
 import com.github.tomakehurst.wiremock.client.WireMock.givenThat
 import com.github.tomakehurst.wiremock.junit5.WireMockRuntimeInfo
 import com.github.tomakehurst.wiremock.junit5.WireMockTest
+import java.time.LocalDate
 import kotlin.test.assertEquals
 import okhttp3.OkHttpClient
 import org.intellij.lang.annotations.Language
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
-import java.time.LocalDate
 
 @WireMockTest
 class AapClientTest {
 
     @Test
-    fun `harAap - har aktiv periode i fremtiden - returnerer true`(wmRuntimeInfo: WireMockRuntimeInfo) {
+    fun `harAap - har aktiv Arena-periode i fremtiden - returnerer true`(wmRuntimeInfo: WireMockRuntimeInfo) {
         val apiUrl = "http://localhost:" + wmRuntimeInfo.httpPort
+        val fraOgMed = LocalDate.now().plusDays(1)
         val tilOgMed = LocalDate.now().plusMonths(6)
         @Language("JSON")
         val response = """
             {
-              "perioder": [
-                { "fraOgMedDato": "2025-01-01", "tilOgMedDato": "$tilOgMed" }
+              "saker": [
+                {
+                  "sakId": "123",
+                  "statusKode": "IVERK",
+                  "periode": {
+                    "fraOgMedDato": "$fraOgMed",
+                    "tilOgMedDato": "$tilOgMed"
+                  },
+                  "kilde": "ARENA"
+                }
               ]
             }
         """.trimIndent()
         givenThat(
-            WireMock.post("/perioder")
+            WireMock.post("/dab/sakerByFnr")
                 .willReturn(
                     WireMock.aResponse()
                         .withStatus(200)
@@ -41,18 +50,27 @@ class AapClientTest {
     }
 
     @Test
-    fun `harAap - periode med tilOgMedDato null - returnerer true`(wmRuntimeInfo: WireMockRuntimeInfo) {
+    fun `harAap - har aktiv Arena-periode med tilOgMedDato null - returnerer true`(wmRuntimeInfo: WireMockRuntimeInfo) {
         val apiUrl = "http://localhost:" + wmRuntimeInfo.httpPort
+        val fraOgMed = LocalDate.now().plusDays(1)
         @Language("JSON")
         val response = """
             {
-              "perioder": [
-                { "fraOgMedDato": "2025-01-01", "tilOgMedDato": null }
+              "saker": [
+                {
+                  "sakId": "123",
+                  "statusKode": "IVERK",
+                  "periode": {
+                    "fraOgMedDato": "$fraOgMed",
+                    "tilOgMedDato": null
+                  },
+                  "kilde": "ARENA"
+                }
               ]
             }
         """.trimIndent()
         givenThat(
-            WireMock.post("/perioder")
+            WireMock.post("/dab/sakerByFnr")
                 .willReturn(
                     WireMock.aResponse()
                         .withStatus(200)
@@ -66,18 +84,28 @@ class AapClientTest {
     }
 
     @Test
-    fun `harAap - kun avsluttede perioder - returnerer false`(wmRuntimeInfo: WireMockRuntimeInfo) {
+    fun `harAap - kun avsluttede Arena-perioder - returnerer false`(wmRuntimeInfo: WireMockRuntimeInfo) {
         val apiUrl = "http://localhost:" + wmRuntimeInfo.httpPort
+        val fraOgMed = LocalDate.now().minusYears(1)
+        val tilOgMed = LocalDate.now().minusWeeks(6)
         @Language("JSON")
         val response = """
             {
-              "perioder": [
-                { "fraOgMedDato": "2024-01-01", "tilOgMedDato": "2024-12-31" }
+              "saker": [
+                {
+                  "sakId": "123",
+                  "statusKode": "IVERK",
+                  "periode": {
+                    "fraOgMedDato": "$fraOgMed",
+                    "tilOgMedDato": "$tilOgMed"
+                  },
+                  "kilde": "ARENA"
+                }
               ]
             }
         """.trimIndent()
         givenThat(
-            WireMock.post("/perioder")
+            WireMock.post("/dab/sakerByFnr")
                 .willReturn(
                     WireMock.aResponse()
                         .withStatus(200)
@@ -91,15 +119,181 @@ class AapClientTest {
     }
 
     @Test
-    fun `harAap - ingen perioder - returnerer false`(wmRuntimeInfo: WireMockRuntimeInfo) {
+    fun `harAap - har aktiv Kelvin-periode i fremtiden - returnerer true`(wmRuntimeInfo: WireMockRuntimeInfo) {
         val apiUrl = "http://localhost:" + wmRuntimeInfo.httpPort
+        val fraOgMed = LocalDate.now().plusDays(1)
+        val tilOgMed = LocalDate.now().plusMonths(6)
+        @Language("JSON")
+        val response = """
+            {
+              "saker": [
+                {
+                  "sakId": "123",
+                  "statuskode": "FERDIGBEHANDLET",
+                  "perioder": [
+                    {
+                      "fraOgMedDato": "$fraOgMed",
+                      "tilOgMedDato": "$tilOgMed"
+                    }
+                  ],
+                  "ytelsesstatus": "LOPENDE",
+                  "kilde": "KELVIN"
+                }
+              ]
+            }
+        """.trimIndent()
         givenThat(
-            WireMock.post("/perioder")
+            WireMock.post("/dab/sakerByFnr")
                 .willReturn(
                     WireMock.aResponse()
                         .withStatus(200)
                         .withHeader("Content-Type", "application/json")
-                        .withBody("""{"perioder": []}""")
+                        .withBody(response)
+                )
+        )
+        val client = AapClient(apiUrl, { "token" }, OkHttpClient.Builder().build())
+
+        assertEquals(true, client.harAap("12345678910"))
+    }
+
+    @Test
+    fun `harAap - har aktiv Kelvin-periode med tilOgMedDato null - returnerer true`(wmRuntimeInfo: WireMockRuntimeInfo) {
+        val apiUrl = "http://localhost:" + wmRuntimeInfo.httpPort
+        val fraOgMed = LocalDate.now().plusDays(1)
+        @Language("JSON")
+        val response = """
+            {
+              "saker": [
+                {
+                  "sakId": "123",
+                  "statuskode": "FERDIGBEHANDLET",
+                  "perioder": [
+                    {
+                      "fraOgMedDato": "$fraOgMed",
+                      "tilOgMedDato": null
+                    }
+                  ],
+                  "ytelsesstatus": "LOPENDE",
+                  "kilde": "KELVIN"
+                }
+              ]
+            }
+        """.trimIndent()
+        givenThat(
+            WireMock.post("/dab/sakerByFnr")
+                .willReturn(
+                    WireMock.aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", "application/json")
+                        .withBody(response)
+                )
+        )
+        val client = AapClient(apiUrl, { "token" }, OkHttpClient.Builder().build())
+
+        assertEquals(true, client.harAap("12345678910"))
+    }
+
+    @Test
+    fun `harAap - kun avsluttede Kelvin-perioder - returnerer false`(wmRuntimeInfo: WireMockRuntimeInfo) {
+        val apiUrl = "http://localhost:" + wmRuntimeInfo.httpPort
+        val fraOgMed = LocalDate.now().minusYears(1)
+        val tilOgMed = LocalDate.now().minusWeeks(6)
+        @Language("JSON")
+        val response = """
+            {
+              "saker": [
+                {
+                  "sakId": "123",
+                  "statuskode": "FERDIGBEHANDLET",
+                  "perioder": [
+                    {
+                      "fraOgMedDato": "$fraOgMed",
+                      "tilOgMedDato": "$tilOgMed"
+                    }
+                  ],
+                  "ytelsesstatus": "LOPENDE",
+                  "kilde": "KELVIN"
+                }
+              ]
+            }
+        """.trimIndent()
+        givenThat(
+            WireMock.post("/dab/sakerByFnr")
+                .willReturn(
+                    WireMock.aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", "application/json")
+                        .withBody(response)
+                )
+        )
+        val client = AapClient(apiUrl, { "token" }, OkHttpClient.Builder().build())
+
+        assertEquals(false, client.harAap("12345678910"))
+    }
+
+    @Test
+    fun `harAap - har avsluttet Arena-periode og mottatt søknad i Kelvin - returnerer true`(wmRuntimeInfo: WireMockRuntimeInfo) {
+        val apiUrl = "http://localhost:" + wmRuntimeInfo.httpPort
+        val fraOgMed = LocalDate.now().minusYears(1)
+        val tilOgMed = LocalDate.now().minusWeeks(6)
+        @Language("JSON")
+        val response = """
+            {
+              "saker": [
+                {
+                  "sakId": "123",
+                  "statusKode": "IVERK",
+                  "periode": {
+                    "fraOgMedDato": "$fraOgMed",
+                    "tilOgMedDato": "$tilOgMed"
+                  },
+                  "kilde": "ARENA"
+                },
+                {
+                  "sakId": "123",
+                  "statuskode": "SOKNAD_UNDER_BEHANDLING",
+                  "perioder": [
+                    {
+                      "fraOgMedDato": null,
+                      "tilOgMedDato": null
+                    }
+                  ],
+                  "ytelsesstatus": "FOR_VEDTAK",
+                  "kilde": "KELVIN"
+                }
+              ]
+            }
+        """.trimIndent()
+        givenThat(
+            WireMock.post("/dab/sakerByFnr")
+                .willReturn(
+                    WireMock.aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", "application/json")
+                        .withBody(response)
+                )
+        )
+        val client = AapClient(apiUrl, { "token" }, OkHttpClient.Builder().build())
+
+        assertEquals(true, client.harAap("12345678910"))
+    }
+
+    @Test
+    fun `harAap - ingen saker - returnerer false`(wmRuntimeInfo: WireMockRuntimeInfo) {
+        val apiUrl = "http://localhost:" + wmRuntimeInfo.httpPort
+        @Language("JSON")
+        val response = """
+            {
+              "saker": []
+            }
+        """.trimIndent()
+        givenThat(
+            WireMock.post("/dab/sakerByFnr")
+                .willReturn(
+                    WireMock.aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", "application/json")
+                        .withBody(response)
                 )
         )
         val client = AapClient(apiUrl, { "token" }, OkHttpClient.Builder().build())
@@ -111,7 +305,7 @@ class AapClientTest {
     fun `harAap - feilrespons fra tjenesten - kaster exception`(wmRuntimeInfo: WireMockRuntimeInfo) {
         val apiUrl = "http://localhost:" + wmRuntimeInfo.httpPort
         givenThat(
-            WireMock.post("/perioder")
+            WireMock.post("/dab/sakerByFnr")
                 .willReturn(
                     WireMock.aResponse()
                         .withStatus(500)
@@ -124,4 +318,3 @@ class AapClientTest {
         }
     }
 }
-
