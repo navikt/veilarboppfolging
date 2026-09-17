c9c3cc30ff3e7c94cc427f7d3ca743b9d0486d70
Tia Firing <tia.firing@nav.no>
2026-09-17 11:50:47 +0200

    kjører kl 12.15 i prod første gang for å få ryddet opp

--- src/main/kotlin/no/nav/veilarboppfolging/kandidatForUtmelding/jobb/KandidatForUtmeldingCron.kt
+++ src/main/kotlin/no/nav/veilarboppfolging/kandidatForUtmelding/jobb/KandidatForUtmeldingCron.kt
@@ -31,7 +31,7 @@ class KandidatForUtmeldingCron(
         }
     }
 
-    @Scheduled(cron = "0 45 11 * * *")
+    @Scheduled(cron = "0 15 12 * * *")
     fun fjernKandidaterSomIkkeKanAvsluttes() {
         if (!leaderElectionClient.isLeader) {
             return
