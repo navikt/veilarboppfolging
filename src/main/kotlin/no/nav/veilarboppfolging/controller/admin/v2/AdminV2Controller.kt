6ceee1851badc3ad01e0bd0da01489b78c21f8c1
sigurdgroneng <sigurd.groneng@nav.no>
2026-09-18 13:16:46 +0200

    Add endpoint for bulk fetch oppfolgingsstatus

--- src/main/kotlin/no/nav/veilarboppfolging/controller/admin/v2/AdminV2Controller.kt
+++ src/main/kotlin/no/nav/veilarboppfolging/controller/admin/v2/AdminV2Controller.kt
@@ -2,11 +2,15 @@ package no.nav.veilarboppfolging.controller.admin.v2
 
 import java.util.UUID
 import no.nav.common.job.JobRunner
+import no.nav.common.types.identer.Fnr
 import no.nav.veilarboppfolging.ForbiddenException
 import no.nav.veilarboppfolging.controller.admin.v1.POAO_ADMIN
+import no.nav.veilarboppfolging.controller.response.AvslutningsStatusDto
 import no.nav.veilarboppfolging.kandidatForUtmelding.RepubliserKandidatForUtmeldingService
 import no.nav.veilarboppfolging.service.AuthService
+import no.nav.veilarboppfolging.service.AvsluttOppfolgingService
 import no.nav.veilarboppfolging.service.KafkaRepubliseringService
+import no.nav.veilarboppfolging.utils.DtoMappers
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -18,6 +22,7 @@ class AdminV2Controller(
     private val authService: AuthService,
     private val kafkaRepubliseringService: KafkaRepubliseringService,
     private val republiserKandidatForUtmeldingService: RepubliserKandidatForUtmeldingService,
+    private val avsluttOppfolgingService: AvsluttOppfolgingService,
 ) {
     @PostMapping("/republiser/oppfolgingsperioder")
     fun republiserOppfolgingsperioder(): String {
@@ -59,8 +64,24 @@ class AdminV2Controller(
         }
     }
 
+    @PostMapping("/avslutning-status")
+    fun hentAvslutningStatusForOppfolgingsperioder(
+        @RequestBody request: HentAvslutningStatusForOppfolgingsperioderRequest
+    ): List<AvslutningsStatusDto> {
+        sjekkTilgangTilAdmin()
+        return request.oppfolgingsperiodeIder.map { oppfolgingsperiodeId ->
+            avsluttOppfolgingService.hentAvslutningstatusForManuellAvslutning(
+                Fnr.of(oppfolgingsperiodeId)
+            ).let { DtoMappers.tilDto(it) }
+        }
+    }
+
     private fun sjekkTilgangTilAdmin() {
         authService.sjekkAtApplikasjonErIAllowList(listOf(POAO_ADMIN))
         if (!authService.erInternBruker()) throw ForbiddenException("Må være internbruker")
     }
 }
+
+data class HentAvslutningStatusForOppfolgingsperioderRequest(
+    val oppfolgingsperiodeIder: List<String>,
+)
